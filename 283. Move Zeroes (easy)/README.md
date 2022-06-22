473266c973fdf4e1a6df27f2d872e91c2ef8ac10
Nadezhda R <nadezhdaryabtsova@gmail.com>
2022-06-22 04:15:10 +0300

    283. Move Zeroes (easy)

---
+++ 283. Move Zeroes (easy)/README.md
@@ -0,0 +1,12 @@
+# Leetcode / 283. Move Zeroes (easy) using C++
+
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+Constraints:
+
+- `1 <= nums.length <= 10^4`
+- `-2^31 <= nums[i] <= 2^31 - 1`
+
+Follow up: Could you minimize the total number of operations done?
---
+++ 283. Move Zeroes (easy)/main.cpp
@@ -0,0 +1,55 @@
+#include <cassert>
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    static void moveZeroes(std::vector<int>& nums) {
+        std::cout << "input: ";
+        for (auto num : nums) {
+            std::cout << num << " ";
+        }
+        std::cout << std::endl;
+
+        int result_nums_idx = -1;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                result_nums_idx++;
+                nums[result_nums_idx] = nums[i];
+            }
+        }
+
+        while (result_nums_idx > -1 && result_nums_idx + 1 < nums.size()) {
+            result_nums_idx++;
+            if (nums[result_nums_idx] != 0)
+                nums[result_nums_idx] = 0;
+        }
+
+        std::cout << "updated: ";
+        for (auto num : nums) {
+            std::cout << num << " ";
+        }
+        std::cout << std::endl;
+    }
+};
+
+int main() {
+    std::vector<int> nums1 = {0,1,0,3,12};
+    std::vector<int> expectedNums1 = {1,3,12,0,0};
+    Solution::moveZeroes(nums1);
+    assert(nums1 == expectedNums1);
+    std::cout << "..........................." << std::endl;
+
+    std::vector<int> nums2 = {0};
+    std::vector<int> expectedNums2 = {0};
+    Solution::moveZeroes(nums1);
+    assert(nums2 == expectedNums2);
+    std::cout << "..........................." << std::endl;
+
+    std::vector<int> nums3 = {0,0,0};
+    std::vector<int> expectedNums3 = {0,0,0};
+    Solution::moveZeroes(nums3);
+    assert(nums3 == expectedNums3);
+    std::cout << "..........................." << std::endl;
+}
