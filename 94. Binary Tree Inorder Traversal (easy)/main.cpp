aa35384411bb66bb6c4fa95df5cca9632274dd18
Nadezhda R <nadezhdaryabtsova@gmail.com>
2022-08-06 07:57:27 +0300

    94. Binary Tree Inorder Traversal (easy)

---
+++ 94. Binary Tree Inorder Traversal (easy)/main.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    std::vector<int> inorderTraversal(TreeNode* root) {
+        nums.reserve(100);
+        traverse(root);
+        nums.shrink_to_fit();
+        return nums;
+    }
+    void traverse(TreeNode* root) {
+        if (root == nullptr)
+            return;
+        if (root->left != nullptr)
+            traverse(root->left);
+        nums.push_back(root->val);
+        if (root->right != nullptr)
+            traverse(root->right);
+    }
+private:
+    std::vector<int> nums = {};
+};
+
+int main() {
+    return 0;
+}
