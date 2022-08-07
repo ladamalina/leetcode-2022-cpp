1ac20e6f6f1874e7b20c97a639e5b7cc984852bf
Nadezhda R <nadezhdaryabtsova@gmail.com>
2022-08-07 12:15:08 +0300

    101. Symmetric Tree (easy)

---
+++ 101. Symmetric Tree (easy)/README.md
@@ -0,0 +1,10 @@
+# Leetcode / 101. Symmetric Tree (easy) using C++
+
+[https://leetcode.com/problems/symmetric-tree/](https://leetcode.com/problems/symmetric-tree/)
+
+Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+Constraints:
+
+- The number of nodes in the tree is in the range `[1, 1000]`.
+- `-100 <= Node.val <= 100`
---
+++ 101. Symmetric Tree (easy)/main.cpp
@@ -0,0 +1,53 @@
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    static bool isSymmetric(TreeNode* root) {
+        if (root->left == nullptr && root->right == nullptr)
+            return true;
+        if (root->left == nullptr || root->right == nullptr)
+            return false;
+        return Solution::isSymmetricSubtrees(root->left, root->right);
+    }
+
+    static bool isSymmetricSubtrees(TreeNode* p, TreeNode* q) {
+        // co,pare roots
+        if (p == nullptr && q == nullptr)
+            return true;
+        if (p == nullptr || q == nullptr)
+            return false;
+        if (p->val != q->val)
+            return false;
+        // p->val == q->val
+
+        if ((p->left == nullptr && q->right != nullptr)
+            || (p->left != nullptr && q->right == nullptr))
+            return false;
+        if (p->left != nullptr && q->right != nullptr && p->val != q->val)
+            return false;
+        bool leftTreeCheck = isSymmetricSubtrees(p->left, q->right);
+        if (!leftTreeCheck)
+            return false;
+
+        if (p->right == nullptr && q->left != nullptr || p->right != nullptr && q->left == nullptr)
+            return false;
+        if (p->right != nullptr && q->left != nullptr && p->val != q->val)
+            return false;
+        bool rightTreeCheck = isSymmetricSubtrees(p->right, q->left);
+        if (!rightTreeCheck)
+            return false;
+
+        return true;
+    }
+};
+
+int main() {
+    return 0;
+}
