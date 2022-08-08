b19fccd81b620abd978be05cf8e7b08c81f25b1e
Nadezhda R <nadezhdaryabtsova@gmail.com>
2022-08-08 06:34:49 +0300

    111. Minimum Depth of Binary Tree (easy)

---
+++ 111. Minimum Depth of Binary Tree (easy)/README.md
@@ -0,0 +1,14 @@
+# Leetcode / 111. Minimum Depth of Binary Tree (easy) using C++
+
+[https://leetcode.com/problems/minimum-depth-of-binary-tree/](https://leetcode.com/problems/minimum-depth-of-binary-tree/)
+
+Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+Note: A leaf is a node with no children.
+
+Constraints:
+
+- The number of nodes in the tree is in the range `[0, 10^5]`.
+- `-1000 <= Node.val <= 1000`
---
+++ 111. Minimum Depth of Binary Tree (easy)/main.cpp
@@ -0,0 +1,42 @@
+#include "algorithm"
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (root == nullptr)
+            return 0;
+        minDepthTraverse(root, 0);
+
+        return minD;
+    }
+
+    void minDepthTraverse(TreeNode* root, int prevD) {
+        // leaf
+        if (root->left == nullptr && root->right == nullptr) {
+            minD = std::min(minD, prevD + 1);
+            return;
+        }
+
+        // not a leaf
+        if (root->left != nullptr)
+            minDepthTraverse(root->left, prevD + 1);
+        if (root->right != nullptr)
+            minDepthTraverse(root->right, prevD + 1);
+    }
+
+private:
+    int minD = 999999;
+};
+
+int main() {
+    return 0;
+}
