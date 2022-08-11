625008034ce3b6e87628267b27383b94319b4482
Nadezhda R <nadezhdaryabtsova@gmail.com>
2022-08-11 07:45:20 +0300

    98. Validate Binary Search Tree (medium)

---
+++ 98. Validate Binary Search Tree (medium)/main.cpp
@@ -0,0 +1,55 @@
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    static bool isValidBST(TreeNode* root) {
+        if (root == nullptr)
+            return true;
+        if (root->left != nullptr && !isValidBSTLeft(root->left, root->val))
+            return false;
+        if (root->right != nullptr && !isValidBSTRight(root->right, root->val))
+            return false;
+        return true;
+    }
+
+    static bool isValidBSTLeft(TreeNode* root, int maxVal) {
+        if (root->val >= maxVal)
+            return false;
+        if (root->left != nullptr && !isValidBSTLeft(root->left, root->val))
+            return false;
+        if (root->right != nullptr && !isValidBSTMiddle(root->right, root->val, maxVal))
+            return false;
+        return true;
+    }
+
+    static bool isValidBSTRight(TreeNode* root, int minVal) {
+        if (root->val <= minVal)
+            return false;
+        if (root->left != nullptr && !isValidBSTMiddle(root->left, minVal, root->val))
+            return false;
+        if (root->right != nullptr && !isValidBSTRight(root->right, root->val))
+            return false;
+        return true;
+    }
+
+    static bool isValidBSTMiddle(TreeNode* root, int minVal, int maxVal) {
+        if (root->val <= minVal || root->val >= maxVal)
+            return false;
+        if (root->left != nullptr && !isValidBSTMiddle(root->left, minVal, root->val))
+            return false;
+        if (root->right != nullptr && !isValidBSTMiddle(root->right, root->val, maxVal))
+            return false;
+        return true;
+    }
+};
+
+int main() {
+    return 0;
+}
