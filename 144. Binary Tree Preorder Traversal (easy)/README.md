edfa4c1b38c56d1944eda84b139903719ee19d1d
Nadezhda R <nadezhdaryabtsova@gmail.com>
2022-08-14 08:48:32 +0300

    144. Binary Tree Preorder Traversal (easy)

---
+++ 144. Binary Tree Preorder Traversal (easy)/README.md
@@ -0,0 +1,12 @@
+# Leetcode / 144. Binary Tree Preorder Traversal (easy) using C++
+
+[https://leetcode.com/problems/binary-tree-preorder-traversal/](https://leetcode.com/problems/binary-tree-preorder-traversal/)
+
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+Constraints:
+
+- The number of nodes in the tree is in the range `[0, 100]`.
+- `-100 <= Node.val <= 100`
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
---
+++ 144. Binary Tree Preorder Traversal (easy)/main.cpp
@@ -0,0 +1,37 @@
+#include <deque>
+#include <vector>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    std::vector<int> preorderTraversal(TreeNode* root) {
+        if (root == nullptr)
+            return {};
+        nums.reserve(100);
+
+        traverse(root);
+        nums.shrink_to_fit();
+        return nums;
+    }
+    void traverse(TreeNode* root) {
+        nums.push_back(root->val);
+        if (root->left != nullptr)
+            traverse(root->left);
+        if (root->right != nullptr)
+            traverse(root->right);
+    }
+private:
+    std::vector<int> nums = {};
+};
+
+int main() {
+    return 0;
+}
