19e7923c68335329f17979bed7f7366eb061dea9
Nadezhda R <nadezhdaryabtsova@gmail.com>
2022-08-15 07:29:22 +0300

    590. N-ary Tree Postorder Traversal (easy)

---
+++ 590. N-ary Tree Postorder Traversal (easy)/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <deque>
+#include <vector>
+
+class Node {
+public:
+    int val;
+    std::vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, std::vector<Node*>& _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+class Solution {
+public:
+    std::vector<int> postorder(Node* root) {
+        if (root == nullptr)
+            return {};
+        postorderTraverse(root);
+        std::vector<int> nodes(nodesQ.begin(), nodesQ.end());
+
+        return nodes;
+    }
+
+    void postorderTraverse(Node* root) {
+        if (!root->children.empty())
+            for (auto& child : root->children)
+                postorderTraverse(child);
+        nodesQ.push_back(root->val);
+    }
+private:
+    std::deque<uint16_t> nodesQ;
+};
+
+int main() {
+    return 0;
+}
