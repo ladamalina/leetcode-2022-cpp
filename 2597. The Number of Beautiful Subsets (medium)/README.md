f6e1f80ecc991b68e370ee748b726eaeb5c1cfe8
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2023-12-25 06:20:06 +0300

    2597. The Number of Beautiful Subsets

---
+++ 2597. The Number of Beautiful Subsets (medium)/README.md
@@ -0,0 +1,3 @@
+# 2597. The Number of Beautiful Subsets
+
+https://leetcode.com/problems/the-number-of-beautiful-subsets
---
+++ 2597. The Number of Beautiful Subsets (medium)/main.cpp
@@ -0,0 +1,95 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define F first
+#define S second
+#define PB push_back
+
+std::bitset<1001> used;
+
+class Solution {
+public:
+  int beautifulSubsets(const vi& a, int k) {
+    const auto n = CI(a.size());
+    const auto max_m = (1<<n)-1;
+    auto res = 0;
+    FOR(m, 1, max_m) {
+      used.reset();
+      auto ok = true;
+      FOR(i, 0, n-1) {
+        if ((m&(1<<i))) {
+          if ((a[i]+k>=1 && a[i]+k<=1000 && used[a[i]+k]) || (a[i]-k>=1 && a[i]-k<=1000 && used[a[i]-k])) {
+            ok = false;
+            break;
+          }
+          used[a[i]] = true;
+        }
+      }
+      res += ok;
+    }
+    return res;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  /*{
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().solve();
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }*/
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
