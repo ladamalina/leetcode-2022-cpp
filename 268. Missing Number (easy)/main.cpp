9e0cf2276db467b08915d0032771dcb8d30f83f5
Nadezhda Ryabtsova <nadezhdaryabtsova@gmail.com>
2024-02-23 22:14:06 +0300

    268. Missing Number (easy)

---
+++ 268. Missing Number (easy)/main.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+
+using namespace std::literals;
+
+using ll [[maybe_unused]] = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vvii [[maybe_unused]] = std::vector<vii>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+using vvs [[maybe_unused]] = std::vector<vs>;
+using vc [[maybe_unused]] = std::vector<char>;
+using vvc [[maybe_unused]] = std::vector<vc>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define F first
+#define S second
+
+class Solution {
+ public:
+  static int missingNumber(const vi& nums) {
+    const auto n = CI(nums.size()) + 1;
+    const auto a_sum = std::accumulate(RNG(nums), 0);
+    const auto e_sum = (n-1)*n/2;
+    return e_sum - a_sum;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const vi nums{3,0,1};
+    const auto e_out = Solution::missingNumber(nums);
+    assert(e_out == 2);
+
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const vi nums{0,1};
+    const auto e_out = Solution::missingNumber(nums);
+    assert(e_out == 2);
+
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const vi nums{9,6,4,2,3,5,7,0,1};
+    const auto e_out = Solution::missingNumber(nums);
+    assert(e_out == 8);
+
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
