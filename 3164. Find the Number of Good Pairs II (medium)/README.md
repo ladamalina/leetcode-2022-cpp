0ba2f87e7a3856221fe3d03721bb94c5ecec0072
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-02-28 06:20:06 +0300

    3164. Find the Number of Good Pairs II (medium)

---
+++ 3164. Find the Number of Good Pairs II (medium)/README.md
@@ -0,0 +1,3 @@
+# 3164. Find the Number of Good Pairs II (medium)
+
+https://leetcode.com/problems/find-the-number-of-good-pairs-ii
---
+++ 3164. Find the Number of Good Pairs II (medium)/main.cpp
@@ -0,0 +1,100 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define F first
+#define S second
+#define PB push_back
+
+template<typename T>
+std::unordered_set<T> GetFactors(T x) {
+  std::unordered_set<T> factors;
+  const auto x_sqrt = std::sqrt(x);
+  for (T i = 1; i <= x_sqrt; ++i) {
+    if (x % i == 0) {
+      factors.insert(i);
+      factors.insert(x/i);
+    }
+  }
+  return factors;
+}
+
+class Solution {
+public:
+  ll numberOfPairs(const vi& nums1, const vi& nums2, int k) {
+    std::unordered_map<int, int> nums2_freq;
+    for (const auto x : nums2) ++nums2_freq[x];
+
+    ll res = 0;
+    for (const auto x : nums1) {
+      if (x%k != 0) continue;
+      const auto fs = GetFactors(x/k);
+      for (const auto f : fs) {
+        if (nums2_freq.contains(f)) res += nums2_freq.at(f);
+      }
+    }
+    return res;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  /*{
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().solve();
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }*/
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
