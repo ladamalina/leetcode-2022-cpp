f30e8042d6632fbd41c0d76e063a92fd86cbb068
Nadezhda Ryabtsova <nadezhdaryabtsova@gmail.com>
2024-03-02 13:18:48 +0300

    2971. Find Polygon With the Largest Perimeter (medium)

---
+++ 2971. Find Polygon With the Largest Perimeter (medium)/README.md
@@ -0,0 +1,3 @@
+# 2971. Find Polygon With the Largest Perimeter (medium)
+
+https://leetcode.com/problems/find-polygon-with-the-largest-perimeter/
---
+++ 2971. Find Polygon With the Largest Perimeter (medium)/main.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+
+using namespace std::literals;
+
+using ll [[maybe_unused]] = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vvii [[maybe_unused]] = std::vector<vii>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+using vvs [[maybe_unused]] = std::vector<vs>;
+using vc [[maybe_unused]] = std::vector<char>;
+using vvc [[maybe_unused]] = std::vector<vc>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define F first
+#define S second
+
+class Solution {
+ public:
+  static ll largestPerimeter(vi& nums) {
+    SORT(nums);
+    ll max_res = -1;
+    ll psum = nums[0]+nums[1];
+    FOR(i, 2, CI(nums.size())-1) {
+      if (nums[i] < psum)
+        max_res = std::max(max_res, psum+nums[i]);
+      psum += nums[i];
+    }
+    return max_res;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    vi nums{5,5,5};
+    const auto a_out = Solution::largestPerimeter(nums);
+    assert(a_out == 15);
+
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    vi nums{1,12,1,2,5,50,3};
+    const auto a_out = Solution::largestPerimeter(nums);
+    assert(a_out == 12);
+
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    vi nums{5,5,50};
+    const auto a_out = Solution::largestPerimeter(nums);
+    assert(a_out == -1);
+
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
