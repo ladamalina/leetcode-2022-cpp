3809b7b2bf86aa602fd0d419b3251e739299c0ab
Nadezhda Ryabtsova <nadezhdaryabtsova@gmail.com>
2024-03-02 13:16:00 +0300

    100. Same Tree (easy)

--- 100. Same Tree (easy)/README.md
+++ 100. Same Tree (easy)/README.md
@@ -1,15 +1,3 @@
-# Leetcode / 100. Same Tree (easy) using C++
+# 100. Same Tree (easy)
 
-[https://leetcode.com/problems/same-tree/](https://leetcode.com/problems/same-tree/)
-
-Given the roots of two binary trees p and q, write a function to check if they are the same or not.
-
-Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
-
-Constraints:
-
-- The number of nodes in both trees is in the range `[0, 100]`.
-- `-10^4 <= Node.val <= 10^4`
-
-
-
+https://leetcode.com/problems/same-tree/
--- 100. Same Tree (easy)/main.cpp
+++ 100. Same Tree (easy)/main.cpp
@@ -1,45 +1,29 @@
-#include <vector>
+#include <bits/stdc++.h>
 
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  explicit TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
 };
 
 class Solution {
-public:
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (!p && !q)
-            return true;
-        if (!p || !q)
-            return false;
-        if (p->val != q->val)
-            return false;
-        // p->val == q->val
+ public:
+  static bool isSameTree(TreeNode* p, TreeNode* q) {
+    if (!p && !q)
+      return true;
+    if (!p || !q)
+      return false;
+    if (p->val != q->val)
+      return false;
+    // p->val == q->val
 
-        if ((!p->left && q->left) || (p->left && !q->left))
-            return false;
-        if (p->left && q->left && p->val != q->val)
-            return false;
-        bool leftTreeCheck = isSameTree(p->left, q->left);
-        if (!leftTreeCheck)
-            return false;
-
-        if ((!p->right && q->right) || (p->right && !q->right))
-            return false;
-        if (p->right && q->right && p->val != q->val)
-            return false;
-        bool rightTreeCheck = isSameTree(p->right, q->right);
-        if (!rightTreeCheck)
-            return false;
-
-        return true;
-    }
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+  }
 };
 
 int main() {
-    return 0;
+  return 0;
 }
