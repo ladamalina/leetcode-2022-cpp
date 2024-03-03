617d064e2a84e2328c741b94186430070fd027ba
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-03-03 22:14:10 +0300

    1750. Minimum Length of String After Deleting Similar Ends (medium)

---
+++ 1750. Minimum Length of String After Deleting Similar Ends (medium)/main.cpp
@@ -0,0 +1,103 @@
+//#pragma GCC optimize("O3")
+//#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+
+using namespace std::literals;
+
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using pll [[maybe_unused]] = std::pair<ll, ll>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<ld>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<ld>(_v)
+#define F first
+#define S second
+#define PB push_back
+
+class Solution {
+public:
+  static int minimumLength(const std::string& s) {
+    int l = 0;
+    int r = CI(s.size())-1;
+    while (l < r && s[l] == s[r]) {
+      while (l+1 < r && s[l] == s[l+1])
+        ++l;
+      while (l < r-1 && s[r-1] == s[r])
+        --r;
+      ++l;
+      --r;
+    }
+    return (l <= r) ? r-l+1 : 0;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const std::string s("ca");
+    const auto a_out = Solution::minimumLength(s);
+    assert(a_out == 2);
+
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const std::string s("cabaabac");
+    const auto a_out = Solution::minimumLength(s);
+    assert(a_out == 0);
+
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const std::string s("aabccabba");
+    const auto a_out = Solution::minimumLength(s);
+    assert(a_out == 3);
+
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const std::string s("bbbbbbbbbbbbbbbbbbbbbbbbbbbabbbbbbbbbbbbbbbccbcbcbccbbabbb");
+    const auto a_out = Solution::minimumLength(s);
+    assert(a_out == 1);
+
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
