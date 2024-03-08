39986ce5583a7b25fd4f0e1a1db749fbdbbaeb6b
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-03-08 22:14:10 +0300

    780. Reaching Points (hard)

---
+++ 780. Reaching Points (hard)/main.cpp
@@ -0,0 +1,136 @@
+//#pragma GCC optimize("O3")
+//#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+
+using namespace std::literals;
+
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using pll [[maybe_unused]] = std::pair<ll, ll>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<ld>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<ld>(_v)
+#define F first
+#define S second
+#define PB push_back
+
+class Solution {
+public:
+  static bool reachingPoints(int sx, int sy, int tx, int ty) {
+    while (sx <= tx && sy <= ty) {
+      if (sx == tx && sy == ty)
+        return true;
+      if (tx > ty) {
+        const auto t_diff = tx - ty;
+        const auto s_diff = tx - sx;
+        if (t_diff < s_diff) {
+          tx -= t_diff%ty==0 ? t_diff/ty*ty : (t_diff/ty+1)*ty;
+        } else {
+          if (s_diff/ty==0)
+            return false;
+          tx -= s_diff/ty*ty;
+        }
+      } else if (tx < ty) {
+        const auto t_diff = ty - tx;
+        const auto s_diff = ty - sy;
+        if (t_diff < s_diff) {
+          ty -= (ty-tx)%tx==0 ? (ty-tx)/tx*tx : ((ty-tx)/tx+1)*tx;
+        } else {
+          if (s_diff/tx==0)
+            return false;
+          ty -= s_diff/tx*tx;
+        }
+      } else {
+        break;
+      }
+    }
+    return false;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const auto a_out = Solution::reachingPoints(1,1,3,5);
+    assert(a_out);
+
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const auto a_out = Solution::reachingPoints(1,1,2,2);
+    assert(!a_out);
+
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const auto a_out = Solution::reachingPoints(1,1,1,1);
+    assert(a_out);
+
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const auto a_out = Solution::reachingPoints(1,17,999999994,17);
+    assert(a_out);
+
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const auto a_out = Solution::reachingPoints(1,3,1000000000,3);
+    assert(a_out);
+
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const auto a_out = Solution::reachingPoints(10,5,15,5);
+    assert(a_out);
+
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
