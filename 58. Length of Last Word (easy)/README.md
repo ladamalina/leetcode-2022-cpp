5459c993c1609933edab9edf01458b958fe4f29c
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-03-31 22:14:06 +0300

    58. Length of Last Word (easy)

--- 58. Length of Last Word (easy)/README.md
+++ 58. Length of Last Word (easy)/README.md
@@ -1,13 +1,3 @@
-# Leetcode / 58. Length of Last Word (easy) using C++
+# 58. Length of Last Word (easy)
 
-[https://leetcode.com/problems/length-of-last-word/](https://leetcode.com/problems/length-of-last-word/)
-
-Given a string s consisting of words and spaces, return the length of the last word in the string.
-
-A word is a maximal substring consisting of non-space characters only.
-
-Constraints:
-
-- `1 <= s.length <= 10^4`
-- s consists of only English letters and spaces ' '.
-- There will be at least one word in s.
+https://leetcode.com/problems/length-of-last-word/
--- 58. Length of Last Word (easy)/main.cpp
+++ 58. Length of Last Word (easy)/main.cpp
@@ -1,56 +1,39 @@
-#include <cassert>
-#include <string>
+#include <bits/stdc++.h>
+
+using namespace std::literals;
 
 class Solution {
-public:
-    static int lengthOfLastWord(const std::string& s) {
-        int s_size = static_cast<int>(s.size());
-        int last_ch_i = static_cast<int>(s.size());
-        for (int i = s_size - 1; i >= 0; --i) {
-            if (s[i] == ' ') {
-                if (last_ch_i == s_size)
-                    continue;
-                else
-                    return last_ch_i - i;
-            } else { // non-space character
-                if (last_ch_i == s_size)
-                    last_ch_i = i;
-                if (i == 0)
-                    return last_ch_i + 1;
-            }
-        }
-
-        return s_size;
+ public:
+  static int lengthOfLastWord(const std::string& s) {
+    const auto n = static_cast<int>(s.size());
+    auto last_i = n;
+    for (auto i = n; i>=0; --i) {
+      if (s[i] == ' ') {
+        if (last_i == n)
+          continue;
+        else
+          return last_i - i;
+      } else { // non-space character
+        if (last_i == n)
+          last_i = i;
+        if (i == 0)
+          return last_i + 1;
+      }
     }
+    return n;
+  }
 };
 
 int main() {
-    const std::string s1 = "Hello World";
-    assert(Solution::lengthOfLastWord(s1) == 5);
+  assert(Solution::lengthOfLastWord("Hello World"s) == 5);
+  assert(Solution::lengthOfLastWord("   fly me   to   the moon  "s) == 4);
+  assert(Solution::lengthOfLastWord("luffy is still joyboy"s) == 6);
+  assert(Solution::lengthOfLastWord("aaa"s) == 3);
+  assert(Solution::lengthOfLastWord("aaa  "s) == 3);
+  assert(Solution::lengthOfLastWord("   aaa"s) == 3);
+  assert(Solution::lengthOfLastWord("a"s) == 1);
+  assert(Solution::lengthOfLastWord("a  "s) == 1);
+  assert(Solution::lengthOfLastWord("   a"s) == 1);
 
-    const std::string s2 = "   fly me   to   the moon  ";
-    assert(Solution::lengthOfLastWord(s2) == 4);
-
-    const std::string s3 = "luffy is still joyboy";
-    assert(Solution::lengthOfLastWord(s3) == 6);
-
-    const std::string s4 = "a";
-    assert(Solution::lengthOfLastWord(s4) == 1);
-
-    const std::string s5 = "aaa";
-    assert(Solution::lengthOfLastWord(s5) == 3);
-
-    const std::string s6 = "aaa  ";
-    assert(Solution::lengthOfLastWord(s6) == 3);
-
-    const std::string s7 = "a  ";
-    assert(Solution::lengthOfLastWord(s7) == 1);
-
-    const std::string s8 = "   a";
-    assert(Solution::lengthOfLastWord(s8) == 1);
-
-    const std::string s9 = "   aaa";
-    assert(Solution::lengthOfLastWord(s9) == 3);
-
-    return 0;
+  return 0;
 }
