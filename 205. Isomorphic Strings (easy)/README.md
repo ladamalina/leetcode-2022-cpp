a81213c283174c8bdb4e1de75540c318b5e2b4b1
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-04-14 22:14:06 +0300

    205. Isomorphic Strings (easy)

--- 205. Isomorphic Strings (easy)/README.md
+++ 205. Isomorphic Strings (easy)/README.md
@@ -1,15 +1,3 @@
-# Leetcode / 205. Isomorphic Strings (easy) using C++
+# 205. Isomorphic Strings (easy)
 
-[https://leetcode.com/problems/reverse-words-in-a-string/](https://leetcode.com/problems/reverse-words-in-a-string/)
-
-Given two strings s and t, determine if they are isomorphic.
-
-Two strings s and t are isomorphic if the characters in s can be replaced to get t.
-
-All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
-
-Constraints:
-
-- `1 <= s.length <= 5 * 10^4`
-- `t.length == s.length`
-- s and t consist of any valid ascii character.
+https://leetcode.com/problems/reverse-words-in-a-string/
--- 205. Isomorphic Strings (easy)/main.cpp
+++ 205. Isomorphic Strings (easy)/main.cpp
@@ -1,41 +1,38 @@
-#include <cassert>
-#include <string>
-#include <unordered_map>
-#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std::literals;
 
 class Solution {
-public:
-    static bool isIsomorphic(std::string s, std::string t) {
-        std::vector<char> repl_s(128);
-        std::vector<char> repl_t(128);
-        for (size_t i = 0; i < s.size(); ++i) {
-            uint8_t s_code = s[i] + 0;
-            uint8_t t_code = t[i] + 0;
-            if (!repl_s[s_code] && !repl_t[t_code]) {
-                repl_s[s_code] = t[i];
-                repl_t[t_code] = s[i];
-                continue;
-            }
-            if (repl_s[s_code] && repl_t[t_code]) {
-                if (repl_s[s_code] == t[i] && repl_t[t_code] == s[i])
-                    continue;
-                return false;
-            }
-            return false;
-        }
-
-        return true;
+  public:
+  static bool isIsomorphic(const std::string& s, const std::string& t) {
+    if (s.size() != t.size())
+      return false;
+    const auto n = static_cast<int>(s.size());
+    std::unordered_map<char, char> st;
+    std::unordered_map<char, char> ts;
+    for (auto i = 0; i < n; ++i) {
+      if (st.contains(s[i])) {
+        if (t[i] != st.at(s[i]))
+          return false;
+      } else {
+        if (ts.contains(t[i]))
+          return false;
+        st[s[i]] = t[i];
+        ts[t[i]] = s[i];
+      }
     }
+    return true;
+  }
 };
 
 int main() {
-    assert(Solution::isIsomorphic("egg", "add"));
-    assert(Solution::isIsomorphic("foo", "bar") == false);
-    assert(Solution::isIsomorphic("paper", "title"));
-    assert(Solution::isIsomorphic("foo", "aaa") == false);
-    assert(Solution::isIsomorphic("qwertyuiop[]asdfghjkl;'\\zxcvbnm,./",
-                                  "',.pyfgcrl/=aoeuidhtns-\\;qjkxbmwvz"));
-    assert(Solution::isIsomorphic("13", "42"));
+  assert(Solution::isIsomorphic("egg"s, "add"s));
+  assert(!Solution::isIsomorphic("foo"s, "bar"s));
+  assert(Solution::isIsomorphic("paper"s, "title"s));
+  assert(!Solution::isIsomorphic("foo"s, "aaa"s));
+  assert(Solution::isIsomorphic("qwertyuiop[]asdfghjkl;'\\zxcvbnm,./"s,
+                                "',.pyfgcrl/=aoeuidhtns-\\;qjkxbmwvz"s));
+  assert(Solution::isIsomorphic("13"s, "42"s));
 
-    return 0;
+  return 0;
 }
