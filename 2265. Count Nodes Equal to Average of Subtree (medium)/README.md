7b9e0ab894f7b2844a99d9f8bfbfc20e22361a0d
Nadezhda R <nadezhdaryabtsova@gmail.com>
2024-06-02 06:20:06 +0300

    2265. Count Nodes Equal to Average of Subtree (medium)

---
+++ 2265. Count Nodes Equal to Average of Subtree (medium)/README.md
@@ -0,0 +1,3 @@
+# 2265. Count Nodes Equal to Average of Subtree (medium)
+
+https://leetcode.com/problems/count-nodes-equal-to-average-of-subtree
---
+++ 2265. Count Nodes Equal to Average of Subtree (medium)/main.cpp
@@ -0,0 +1,108 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  explicit TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+  int averageOfSubtree(TreeNode* root) {
+    auto res = 0;
+    std::unordered_map<TreeNode*, ii> node_freq_sum;
+    const std::function<void(TreeNode*)> dfs = [&](TreeNode* node) {
+      auto& freq_sum = node_freq_sum[node];
+      ++freq_sum.F;
+      freq_sum.S += node->val;
+      if (node->left) {
+        dfs(node->left);
+        freq_sum.F += node_freq_sum[node->left].F;
+        freq_sum.S += node_freq_sum[node->left].S;
+      }
+      if (node->right) {
+        dfs(node->right);
+        freq_sum.F += node_freq_sum[node->right].F;
+        freq_sum.S += node_freq_sum[node->right].S;
+      }
+      res += (freq_sum.S / freq_sum.F == node->val);
+    };
+    dfs(root);
+    return res;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  /*{
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().solve();
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }*/
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
