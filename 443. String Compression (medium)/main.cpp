700f2625ffbecfcadd2d09fd002dae59365da0bd
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-07-10 06:20:06 +0300

    443. String Compression (medium)

--- 443. String Compression (medium)/main.cpp
+++ 443. String Compression (medium)/main.cpp
@@ -1,70 +1,131 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
 #include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
 #include <vector>
 
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
 class Solution {
- public:
-  static int compress(std::vector<char>& chars) {
-    if (chars.size() == 1u) {
-      return 1;
-    }
-    int code_end_idx = -1;
-    int current_char_idx = 0;
-    int current_char_len = 1;
-    for (int i = 0; i + 1 < static_cast<int>(chars.size()); ++i) {
-      if (chars[i] == chars[i + 1]) {
-        ++current_char_len;
-        continue;
-      } // chars[i] != chars[i + 1]
-
-      insertCode(chars, code_end_idx, current_char_idx, current_char_len);
-      current_char_idx = i + 1;
-      current_char_len = 1;
-    }
-    if (current_char_len > 0) {
-      insertCode(chars, code_end_idx, current_char_idx, current_char_len);
-    }
-
-    return code_end_idx + 1;
-  }
-
-  static void insertCode(std::vector<char>& chars, int& code_end_idx, const int current_char_idx,
-                           const int current_char_len) {
-    chars[++code_end_idx] = chars[current_char_idx];
-    if (current_char_len > 1) {
-      std::string code_len_str = std::to_string(current_char_len);
-      for (const auto ch : code_len_str) {
-        chars[++code_end_idx] = ch;
+public:
+  int compress(std::vector<char>& chars) {
+    const auto n = SZ(chars);
+    auto ii = 1, oi = 0, bi = 0;
+    while (ii <= n) {
+      if (ii < n && chars[ii] == chars[bi]) {
+        // 
+      } else {
+        auto len = ii - bi;
+        chars[oi] = chars[bi]; ++oi;
+        if (len == 1) {
+          // do nothing
+        } else if (len <= 9) {
+          chars[oi] = '0'+len; ++oi;
+        } else if (len <= 99) {
+          chars[oi] = '0'+len/10; ++oi; len %= 10;
+          chars[oi] = '0'+len; ++oi;
+        } else if (len <= 999) {
+          chars[oi] = '0'+len/100; ++oi; len %= 100;
+          chars[oi] = '0'+len/10; ++oi; len %= 10;
+          chars[oi] = '0'+len; ++oi;
+        } else {
+          chars[oi] = '0'+len/1000; ++oi; len %= 1000;
+          chars[oi] = '0'+len/100; ++oi; len %= 100;
+          chars[oi] = '0'+len/10; ++oi; len %= 10;
+          chars[oi] = '0'+len; ++oi;
+        }
+        bi = ii;
       }
+      ++ii;
     }
+    return oi;
   }
 };
 
-int main() {
+[[maybe_unused]] void TestSolution() {
   {
-    std::vector<char> chars{'a','a','b','b','c','c','c'};
-    std::vector<char> expected{'a','2','b','2','c','3'};
-    assert(Solution::compress(chars) == 6);
-    for (int i = 0; i < static_cast<int>(expected.size()); ++i) {
-      assert(chars[i] == expected[i]);
-    }
-  }
-  {
-    std::vector<char> chars{'a'};
-    std::vector<char> expected{'a'};
-    assert(Solution::compress(chars) == 1);
-    for (int i = 0; i < static_cast<int>(expected.size()); ++i) {
-      assert(chars[i] == expected[i]);
-    }
-  }
-  {
-    std::vector<char> chars{'a','b','b','b','b','b','b','b','b','b','b','b','b'};
-    std::vector<char> expected{'a','b','1','2'};
-    assert(Solution::compress(chars) == 4);
-    for (int i = 0; i < static_cast<int>(expected.size()); ++i) {
-      assert(chars[i] == expected[i]);
-    }
-  }
+    const auto start_t = std::chrono::high_resolution_clock::now();
 
+    std::vector<char> chars{'a','a','b','b','c','c','c'};
+    const auto a_out = Solution().compress(chars);
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    std::vector<char> chars{'a'};
+    const auto a_out = Solution().compress(chars);
+    assert(a_out == 1);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    std::vector<char> chars{'a','b','b','b','b','b','b','b','b','b','b','b','b'};
+    const auto a_out = Solution().compress(chars);
+    assert(a_out == 4);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
   return 0;
 }
