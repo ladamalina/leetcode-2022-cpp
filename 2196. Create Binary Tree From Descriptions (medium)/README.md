b004d974cb7d5be302119512413cf9406fde6d05
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-07-15 09:47:47 +0300

    2196. Create Binary Tree From Descriptions (medium)

---
+++ 2196. Create Binary Tree From Descriptions (medium)/README.md
@@ -0,0 +1,3 @@
+# 2196. Create Binary Tree From Descriptions (medium)
+
+https://leetcode.com/problems/create-binary-tree-from-descriptions
---
+++ 2196. Create Binary Tree From Descriptions (medium)/main.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+
+/*#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>*/
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define F first
+#define S second
+
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  explicit TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+  TreeNode* createBinaryTree(const vvi& descriptions) {
+    std::unordered_set<int> parents, children;
+    std::unordered_map<int, TreeNode*> nodes;
+    for (const auto& d : descriptions) {
+      if (!nodes.contains(d[0])) nodes[d[0]] = new TreeNode(d[0]);
+      if (!nodes.contains(d[1])) nodes[d[1]] = new TreeNode(d[1]);
+      if (d[2]) nodes[d[0]]->left = nodes[d[1]];
+      else nodes[d[0]]->right = nodes[d[1]];
+      parents.insert(d[0]);
+      children.insert(d[1]);
+    }
+    auto root_val = -1;
+    for (const auto p : parents)
+      if (!children.contains(p)) root_val = p;
+    return nodes[root_val];
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  /*{
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().solve();
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }*/
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
