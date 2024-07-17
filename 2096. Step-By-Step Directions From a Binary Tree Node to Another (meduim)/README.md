d9310900f49ac756dfbdcfe48d4769db69aebe9a
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-07-17 07:44:00 +0300

    2096. Step-By-Step Directions From a Binary Tree Node to Another (medium)

--- 2096. Step-By-Step Directions From a Binary Tree Node to Another (meduim)/README.md
+++ 2096. Step-By-Step Directions From a Binary Tree Node to Another (meduim)/README.md
@@ -1,30 +1,3 @@
-# Leetcode / 2096. Step-By-Step Directions From a Binary Tree Node to Another (medium) using C++
+# 2096. Step-By-Step Directions From a Binary Tree Node to Another (medium)
 
-[https://leetcode.com/problems/step-by-step-directions-from-a-binary-tree-node-to-another/](https://leetcode.com/problems/step-by-step-directions-from-a-binary-tree-node-to-another/)
-
-You are given the root of a binary tree with n nodes. Each node is uniquely assigned a value from 1 to n. You are also given an integer startValue representing the value of the start node s, and a different integer destValue representing the value of the destination node t.
-
-Find the shortest path starting from node s and ending at node t. Generate step-by-step directions of such path as a string consisting of only the uppercase letters 'L', 'R', and 'U'. Each letter indicates a specific direction:
-
-- 'L' means to go from a node to its left child node.
-- 'R' means to go from a node to its right child node.
-- 'U' means to go from a node to its parent node.
-
-Return the step-by-step directions of the shortest path from node s to node t.
-
-![Leetcode / 2096. Step-By-Step Directions From a Binary Tree Node to Another (medium)](eg1.png)
-
-```
-Input: root = [5,1,2,3,null,6,4], startValue = 3, destValue = 6
-Output: "UURL"
-Explanation: The shortest path is: 3 → 1 → 5 → 2 → 6.
-```
-
-Constraints:
-
-- The number of nodes in the tree is n.
-- `2 <= n <= 10^5`
-- `1 <= Node.val <= n`
-- All the values in the tree are unique.
-- `1 <= startValue, destValue <= n`
-- `startValue != destValue`
+https://leetcode.com/problems/step-by-step-directions-from-a-binary-tree-node-to-another
--- 2096. Step-By-Step Directions From a Binary Tree Node to Another (meduim)/main.cpp
+++ 2096. Step-By-Step Directions From a Binary Tree Node to Another (meduim)/main.cpp
@@ -1,112 +1,111 @@
-#include <algorithm>
-#include <iostream>
-#include <list>
-#include <string>
+#include <bits/stdc++.h>
 
+/*#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>*/
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define F first
+#define S second
 
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-typedef std::list<std::pair<TreeNode*, char>> Path;
+bool DFS(int dst, TreeNode* node, std::string& path) {
+  if (node->val == dst) return true;
+  if (node->left && DFS(dst, node->left, path)) {
+    path.push_back('L');
+    return true;
+  }
+  if (node->right && DFS(dst, node->right, path)) {
+    path.push_back('R');
+    return true;
+  }
+  return false;
+}
 
 class Solution {
 public:
-  std::string getDirections(TreeNode* root, int startValue, int destValue) {
-    findPathToNode(root, startValue, start_val_path, true);
-    findPathToNode(root, destValue, dest_val_path, true);
+  std::string getDirections(TreeNode* root, int src, int dst) {
+    std::string src_path, dst_path;
+    DFS(src, root, src_path);
+    DFS(dst, root, dst_path);
 
-    std::string direction;
-    direction.reserve(36);
-
-    // find the lowest common parent
-    auto start_val_it = start_val_path.begin();
-    auto dest_val_it = dest_val_path.begin();
-    while (start_val_it->first->val == dest_val_it->first->val) {
-//      std::cout << start_val_it->first->val << " == " << dest_val_it->first->val << "\n";
-      start_val_path.erase(start_val_path.begin(), start_val_it);
-      dest_val_path.erase(dest_val_path.begin(), dest_val_it);
-
-      start_val_it = std::next(start_val_it);
-      dest_val_it = std::next(dest_val_it);
-      if (start_val_it == start_val_path.end() || dest_val_it == dest_val_path.end())
-        break;
+    while (!src_path.empty() && !dst_path.empty() && src_path.back() == dst_path.back()) {
+      src_path.pop_back();
+      dst_path.pop_back();
     }
-    auto start_common_parent = std::prev(start_val_it);
-    auto dest_common_parent = std::prev(dest_val_it);
 
-//    std::cout << "start_common_parent: " << start_common_parent->first->val << "\n";
-//    std::cout << "dest_common_parent: " << dest_common_parent->first->val << "\n";
-
-    std::for_each(start_common_parent, std::prev(start_val_path.end()),
-                  [&direction](std::pair<TreeNode*, char>& step){ direction += 'U'; });
-
-    std::for_each(std::next(dest_common_parent), dest_val_path.end(),
-                  [&direction](std::pair<TreeNode*, char>& step){ direction += step.second; });
-
-    return direction;
+    std::reverse(RNG(dst_path));
+    const auto res = (src_path.empty() ? "" : std::string(src_path.size(), 'U')) + dst_path;
+    return res;
   }
-
-  bool findPathToNode(TreeNode* root, int& node_val, Path& path_to_append, bool to_left) {
-    if (root->val == node_val) {
-      if (to_left)
-        path_to_append.push_back({root, 'L'});
-      else
-        path_to_append.push_back({root, 'R'});
-      return true;
-    }
-
-    if (root->left) {
-      auto found_on_left = findPathToNode(root->left, node_val, path_to_append, true);
-      if (found_on_left) {
-        if (to_left)
-          path_to_append.push_front({root, 'L'});
-        else
-          path_to_append.push_front({root, 'R'});
-        return true;
-      }
-    }
-
-    if (root->right) {
-      auto found_on_right = findPathToNode(root->right, node_val, path_to_append, false);
-      if (found_on_right) {
-        if (to_left)
-          path_to_append.push_front({root, 'L'});
-        else
-          path_to_append.push_front({root, 'R'});
-        return true;
-      }
-    }
-
-    // leaf or path not found in subtree
-    return false;
-  }
-private:
-  Path start_val_path;
-  Path dest_val_path;
 };
 
+[[maybe_unused]] void TestSolution() {
+  /*{
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().solve();
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }*/
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
 int main() {
-  auto t1_3 = TreeNode(3);
-  auto t1_1 = TreeNode(1, &t1_3, nullptr);
-  auto t1_6 = TreeNode(6);
-  auto t1_4 = TreeNode(4);
-  auto t1_2 = TreeNode(2, &t1_6, &t1_4);
-  auto t1_5 = TreeNode(5, &t1_1, &t1_2);
-  std::string expected_directions1 = "UURL";
-  std::string actual_directions1 = Solution().getDirections(&t1_5, 3, 6);
-  assert(actual_directions1 == expected_directions1);
-
-  auto t2_1 = TreeNode(1);
-  auto t2_2 = TreeNode(2, &t2_1, nullptr);
-  std::string expected_directions2 = "L";
-  std::string actual_directions2 = Solution().getDirections(&t2_2, 2, 1);
-  assert(actual_directions2 == expected_directions2);
-
+#ifndef NDEBUG
+  TestSolution();
+#endif
   return 0;
 }
