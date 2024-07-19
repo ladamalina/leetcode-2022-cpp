a7bea622eb50e620c3855cf8996232311974e851
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-07-19 06:20:06 +0300

    2192. All Ancestors of a Node in a Directed Acyclic Graph (medium)

---
+++ 2192. All Ancestors of a Node in a Directed Acyclic Graph (medium)/README.md
@@ -0,0 +1,3 @@
+# 2192. All Ancestors of a Node in a Directed Acyclic Graph (medium)
+
+https://leetcode.com/problems/all-ancestors-of-a-node-in-a-directed-acyclic-graph
---
+++ 2192. All Ancestors of a Node in a Directed Acyclic Graph (medium)/main.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define F first
+#define S second
+
+class Solution {
+public:
+  vvi getAncestors(int n, const vvi& edges) {
+    vvi g(n);
+    for (const auto& e : edges) {
+      g[e[1]].push_back(e[0]);
+    }
+    vvi res(n);
+    vb vis(n);
+    const std::function<void(int,int)> dfs = [&](int v, int p) {
+      if (vis[v]) return;
+      vis[v] = true;
+      if (v != p) res[p].push_back(v);
+      for (const auto u : g[v])
+        dfs(u, p);
+    };
+    FOR(v, 0, n-1) {
+      std::fill(RNG(vis), false);
+      dfs(v, v);
+      SORT(res[v]);
+    }
+    return res;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  /*{
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().solve();
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }*/
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
