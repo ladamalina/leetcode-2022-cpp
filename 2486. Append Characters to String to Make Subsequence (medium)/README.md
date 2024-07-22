382f4a70fad4a74a89a6d4f220f89dcd8fcf1ee9
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-07-22 06:20:06 +0300

    2486. Append Characters to String to Make Subsequence (medium)

---
+++ 2486. Append Characters to String to Make Subsequence (medium)/README.md
@@ -0,0 +1,3 @@
+# 2486. Append Characters to String to Make Subsequence (medium)
+
+https://leetcode.com/problems/append-characters-to-string-to-make-subsequence
---
+++ 2486. Append Characters to String to Make Subsequence (medium)/main.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+
+/*#include <algorithm>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>*/
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define F first
+#define S second
+#define PB push_back
+
+class Solution {
+public:
+  int appendCharacters(const std::string& s, const std::string& t) {
+    auto s_it = s.begin();
+    auto t_it = t.begin();
+    while (s_it < s.end() && t_it < t.end()) {
+      if (*s_it == *t_it) {
+        ++t_it;
+      }
+      ++s_it;
+    }
+    return t.end() - t_it;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().appendCharacters("coaching"s, "coding"s);
+    assert(a_out == 4);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().appendCharacters("abcde"s, "a"s);
+    assert(a_out == 0);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().appendCharacters("z"s, "abcde"s);
+    assert(a_out == 5);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
