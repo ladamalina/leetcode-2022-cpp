e5ede7ab231339fbedeae6e226d2ecb4649a26e6
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-08-24 06:20:06 +0300

    2349. Design a Number Container System (medium)

---
+++ 2349. Design a Number Container System (medium)/README.md
@@ -0,0 +1,3 @@
+# 2349. Design a Number Container System (medium)
+
+https://leetcode.com/problems/design-a-number-container-system
---
+++ 2349. Design a Number Container System (medium)/main.cpp
@@ -0,0 +1,107 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vvl [[maybe_unused]] = std::vector<vl>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+class NumberContainers {
+public:
+  NumberContainers() = default;
+    
+  void change(const int i, const int x) {
+    if (idx_x.contains(i)) {
+      const auto old_x = idx_x.at(i);
+      if (old_x != x) {
+        x_ids[old_x].erase(i);
+        if (x_ids[old_x].empty())
+          x_ids.erase(old_x);
+      }
+    }
+    idx_x[i] = x;
+    x_ids[x].insert(i);
+  }
+    
+  int find(const int x) {
+    return x_ids.contains(x) ? *x_ids.at(x).begin() : -1;
+  }
+
+private:
+  std::unordered_map<int, int> idx_x;
+  std::unordered_map<int, std::set<int>> x_ids;
+};
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers* obj = new NumberContainers();
+ * obj->change(index,number);
+ * int param_2 = obj->find(number);
+ */
+
+[[maybe_unused]] void TestSolution() {
+  /*{
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().solve();
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }*/
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
