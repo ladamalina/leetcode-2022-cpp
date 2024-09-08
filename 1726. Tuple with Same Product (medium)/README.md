fd5098b0e97f5290870f1eb6f454340d4004b1cc
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-09-08 06:20:06 +0300

    1726. Tuple with Same Product (medium)

---
+++ 1726. Tuple with Same Product (medium)/README.md
@@ -0,0 +1,3 @@
+# 1726. Tuple with Same Product (medium)
+
+https://leetcode.com/problems/tuple-with-same-product
---
+++ 1726. Tuple with Same Product (medium)/main.cpp
@@ -0,0 +1,117 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vvl [[maybe_unused]] = std::vector<vl>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+class Solution {
+public:
+  int tupleSameProduct(const vi& nums) {
+    const auto n = SZ(nums);
+
+    vvi a(n, vi(n));
+    std::unordered_map<int, int> p_freq;
+    std::vector<std::unordered_map<int, int>> row_p_freq(n), col_p_freq(n);
+    FOR(i, 0, n-1) {
+      FOR(j, i+1, n-1) {
+        const auto p = nums[i] * nums[j];
+        a[i][j] = p;
+        ++p_freq[p];
+        ++row_p_freq[i][p];
+        ++col_p_freq[j][p];
+      }
+    }
+
+    auto res = 0;
+    FOR(i, 0, n-1) {
+      FOR(j, i+1, n-1) {
+        const auto p = a[i][j];
+        const auto exc_cnt = row_p_freq[i][p] + col_p_freq[j][p] - 1;
+        res += (p_freq[p] - exc_cnt) * 4;
+      }
+    }
+
+    return res;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const vi nums{2,3,4,6};
+    const auto a_out = Solution().tupleSameProduct(nums);
+    assert(a_out == 8);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const vi nums{1,2,4,5,10};
+    const auto a_out = Solution().tupleSameProduct(nums);
+    assert(a_out == 16);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
