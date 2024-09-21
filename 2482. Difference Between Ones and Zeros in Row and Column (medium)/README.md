809b8a69d84fd969501b74a0dd2fa856f09d0ccb
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-09-21 06:20:06 +0300

    2482. Difference Between Ones and Zeros in Row and Column (medium)

---
+++ 2482. Difference Between Ones and Zeros in Row and Column (medium)/README.md
@@ -0,0 +1,3 @@
+# 2482. Difference Between Ones and Zeros in Row and Column (medium)
+
+https://leetcode.com/problems/difference-between-ones-and-zeros-in-row-and-column
---
+++ 2482. Difference Between Ones and Zeros in Row and Column (medium)/main.cpp
@@ -0,0 +1,108 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+constexpr int MAX_N = 1e5;
+std::array<int, MAX_N> rows0, rows1, cols0, cols1;
+
+class Solution {
+public:
+  vvi onesMinusZeros(vvi& grid) {
+    const auto n = SZ(grid), m = SZ(grid[0]);
+
+    std::fill(rows0.begin(), rows0.begin()+n, 0);
+    std::fill(rows1.begin(), rows1.begin()+n, 0);
+    std::fill(cols0.begin(), cols0.begin()+m, 0);
+    std::fill(cols1.begin(), cols1.begin()+m, 0);
+
+    FOR(i, 0, n-1) {
+      FOR(j, 0, m-1) {
+        if (grid[i][j] == 0) {
+          ++rows0[i];
+          ++cols0[j];
+        } else {
+          ++rows1[i];
+          ++cols1[j];
+        }
+      }
+    }
+
+    FOR(i, 0, n-1) {
+      FOR(j, 0, m-1) {
+        grid[i][j] = (rows1[i] + cols1[j]) - (rows0[i] + cols0[j]);
+      }
+    }
+    
+    return grid;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  /*{
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().solve();
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }*/
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
