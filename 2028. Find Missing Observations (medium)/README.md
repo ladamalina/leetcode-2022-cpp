ff09880a46ff2b3cf9655c99d7af0548dde965be
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-09-29 13:32:54 +0300

    2028. Find Missing Observations (medium)

---
+++ 2028. Find Missing Observations (medium)/README.md
@@ -0,0 +1,3 @@
+# 2028. Find Missing Observations (medium)
+
+https://leetcode.com/problems/find-missing-observations
---
+++ 2028. Find Missing Observations (medium)/main.cpp
@@ -0,0 +1,95 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+class Solution {
+public:
+  vi missingRolls(const vi& m_rolls, int mean, int n) {
+    const auto m = SZ(m_rolls);
+    const auto m_sum = std::accumulate(RNG(m_rolls), 0);
+    const auto nm_sum = mean * (n + m);
+    const auto n_sum = nm_sum - m_sum;
+    vi res(n, 1);
+    auto c_sum = n;
+    FOR(i, 0, n-1) {
+      const auto add = std::min(5, n_sum-c_sum);
+      if (add > 0) {
+        res[i] += add;
+        c_sum += add;
+      } else {
+        break;
+      }
+    }
+    return c_sum == n_sum ? res : vi{};
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  /*{
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().solve();
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }*/
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
