417c1fd414a171651bede5070ab1a8db0d204f6c
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-10-05 23:46:17 +0300

    1590. Make Sum Divisible by P (medium)

---
+++ 1590. Make Sum Divisible by P (medium)/main.cpp
@@ -0,0 +1,138 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+class Solution {
+public:
+  int minSubarray(const vi& nums, int p) {
+    const auto n = SZ(nums);
+
+    const int target_mod = std::accumulate(nums.begin(), nums.end(), 0ll) % p;
+    if (target_mod == 0)
+      return 0;
+
+    auto pref_mod = 0;
+    std::unordered_map<int, std::deque<int>> mod_ps;
+    for (auto i = 0; i < n; ++i) {
+      const auto mod = (pref_mod + nums[i]) % p;
+      mod_ps[mod].push_back(i);
+      pref_mod = mod;
+    }
+
+    auto base_mod = 0;
+    auto min_len = n;
+    for (auto i = 0; i < n; ++i) {
+      const auto x = (target_mod + base_mod) % p;
+      if (mod_ps.contains(x)) {
+        auto& ps = mod_ps.at(x);
+        while (!ps.empty() && ps.front() < i)
+          ps.pop_front();
+        if (!ps.empty()) {
+          min_len = std::min(min_len, ps.front() - i + 1);
+        }
+      }
+      base_mod = (base_mod + nums[i]) % p;
+    }
+    return (min_len == n) ? -1 : min_len;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().minSubarray({3,1,4,2}, 6);
+    assert(a_out == 1);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().minSubarray({6,3,5,2}, 9);
+    assert(a_out == 2);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().minSubarray({1,2,3}, 3);
+    assert(a_out == 0);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().minSubarray({1,2,3}, 7);
+    assert(a_out == -1);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
