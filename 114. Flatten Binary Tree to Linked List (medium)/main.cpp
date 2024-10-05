25425b08fec06722eef409fc2eb1b66904c187aa
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-10-06 01:36:08 +0300

    114. Flatten Binary Tree to Linked List (medium)

---
+++ 114. Flatten Binary Tree to Linked List (medium)/main.cpp
@@ -0,0 +1,105 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  explicit TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+  void flatten(TreeNode* root) {
+    if (!root)
+      return;
+    auto head = new TreeNode();
+    DFS(root, head);
+    root->left = nullptr;
+    root->right = head->right->right;
+  }
+private:
+  TreeNode* DFS(TreeNode* root, TreeNode* tail) {
+    if (!root)
+      return tail;
+    tail->right = new TreeNode(root->val);
+    tail = DFS(root->left, tail->right);
+    return DFS(root->right, tail);
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    auto root = new TreeNode(1,
+      new TreeNode(2, new TreeNode(3), new TreeNode(4)),
+      new TreeNode(5, nullptr, new TreeNode(6)));
+    Solution().flatten(root);
+    // assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
