7e9f3905696daa5c42ccc28a84bc6219c8614774
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-10-09 06:20:06 +0300

    241. Different Ways to Add Parentheses (medium)

---
+++ 241. Different Ways to Add Parentheses (medium)/main.cpp
@@ -0,0 +1,110 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+class Solution {
+public:
+  vi diffWaysToCompute(const std::string& exp) {
+    const auto n = SZ(exp);
+    vi res;
+    FOR(i, 0, n-1) {
+      const auto op = exp[i];
+      if (op == '+' || op == '-' || op == '*') {
+        const auto s1 = diffWaysToCompute(exp.substr(0, i));
+        const auto s2 = diffWaysToCompute(exp.substr(i+1));
+        for (const auto a : s1) {
+          for (const auto b : s2) {
+            if (op == '+') res.push_back(a + b);
+            else if (op == '-') res.push_back(a - b);
+            else if (op == '*') res.push_back(a * b);
+          }
+        }
+      }
+    }
+    if (res.empty())
+      res.push_back(std::stoi(exp));
+    return res;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const auto a_out = Solution().diffWaysToCompute("2-1-1");
+    const vi e_out{0, 2};
+    assert(a_out == e_out);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const auto a_out = Solution().diffWaysToCompute("2*3-4*5");
+    const vi e_out{-34,-14,-10,-10,10};
+    assert(a_out == e_out);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
