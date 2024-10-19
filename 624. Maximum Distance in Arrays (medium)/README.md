9170a1520da9ba2328adf9825725c463e71f126e
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-10-19 16:06:13 +0300

    624. Maximum Distance in Arrays (medium)

---
+++ 624. Maximum Distance in Arrays (medium)/README.md
@@ -0,0 +1,3 @@
+# 624. Maximum Distance in Arrays (medium)
+
+https://leetcode.com/problems/maximum-distance-in-arrays
---
+++ 624. Maximum Distance in Arrays (medium)/main.cpp
@@ -0,0 +1,95 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+class Solution {
+public:
+  int maxDistance(const vvi& arrays) {
+    const auto n = SZ(arrays);
+    std::set<ii> mn, mx;
+    FOR(i, 0, n-1) {
+      mn.emplace(arrays[i].front(), i);
+      mx.emplace(arrays[i].back(), i);
+    }
+    auto max_d = 0;
+    FOR(i, 0, n-1) {
+      const auto oth_mx = std::prev(mx.end())->S == i ?
+        std::prev(std::prev(mx.end()))->F : std::prev(mx.end())->F;
+      max_d = std::max(max_d, std::abs(oth_mx - arrays[i].front()));
+      const auto oth_mn = mx.begin()->S == i ?
+        std::next(mx.begin())->F : mx.begin()->F;
+      max_d = std::max(max_d, std::abs(oth_mn - arrays[i].back()));
+    }
+    return max_d;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  /*{
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().solve();
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }*/
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
