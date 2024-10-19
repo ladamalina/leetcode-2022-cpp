62729384745e8e73ba277d017e668bf32959bedc
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-10-19 16:05:36 +0300

    664. Strange Printer (hard)

---
+++ 664. Strange Printer (hard)/main.cpp
@@ -0,0 +1,113 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+class Solution {
+public:
+  int strangePrinter(const std::string& s) {
+    const auto n = SZ(s);
+    vvi dp(n, vi(n, -1));
+    return Rec(0, n-1, s, dp);
+  }
+private:
+  int Rec(const int i, const int j, const std::string& s, vvi& dp) {
+    if (i > j)
+      return 0;
+    if (dp[i][j] != -1)
+      return dp[i][j];
+    
+    const auto c1 = s[i];
+    // If the current character is not repeated in the rest of the string
+    auto res = 1 + Rec(i+1, j, s, dp);
+    FOR(k, i+1, j) {
+      // If repeated then update the answer
+      if (s[k] == c1) {
+        // Splitting from i -> k - 1 (remove the last character)
+        // and from k + 1 -> j
+        res = std::min(res, Rec(i, k-1, s, dp) + Rec(k+1, j, s, dp));
+      }
+    }
+    dp[i][j] = res;
+    return res;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().strangePrinter("aaabbb"s);
+    assert(a_out == 2);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().strangePrinter("aba"s);
+    assert(a_out == 2);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
