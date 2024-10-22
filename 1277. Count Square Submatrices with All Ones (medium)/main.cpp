e9a01d7e4f67c0cec2f6a110b6f33d2c06640e33
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-10-22 06:20:06 +0300

    1277. Count Square Submatrices with All Ones (medium)

---
+++ 1277. Count Square Submatrices with All Ones (medium)/main.cpp
@@ -0,0 +1,106 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+class Solution {
+public:
+  int countSquares(const vvi& matrix) {
+    const auto m = SZ(matrix);
+    const auto n = SZ(matrix[0]);
+    vvi g(m, vi(n));
+    FOR(i, 0, m-1) {
+      FOR(j, 0, n-1) {
+        g[i][j] = matrix[i][j] + (i>0 ? g[i-1][j] : 0) + (j>0 ? g[i][j-1] : 0) - (i>0 && j>0 ? g[i-1][j-1] : 0);
+      }
+    }
+    int res = 0;
+    FOR(i, 0, m-1) {
+      FOR(j, 0, n-1) {
+        if (matrix[i][j] == 1) {
+          auto l = 1, r = std::min(m-i, n-j);
+          while (l < r) {
+            const auto k = (l + r + 1) / 2;
+            const auto i2 = i + k - 1, j2 = j + k - 1;
+            const auto k_sum = g[i2][j2] - (i>0 ? g[i-1][j2] : 0) - (j>0 ? g[i2][j-1] : 0) + (i>0 && j>0 ? g[i-1][j-1] : 0);
+            if (k_sum == k*k)
+              l = k;
+            else
+              r = k - 1;
+          }
+          res += r;
+        }
+      }
+    }
+    return res;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  /*{
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().solve();
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }*/
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
