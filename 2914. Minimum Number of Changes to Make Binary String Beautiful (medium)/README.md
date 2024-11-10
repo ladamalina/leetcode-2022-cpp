59eb7acad0c66e84b8a46339dd8036429f66aec1
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-11-10 06:20:06 +0300

    2914. Minimum Number of Changes to Make Binary String Beautiful (medium)

---
+++ 2914. Minimum Number of Changes to Make Binary String Beautiful (medium)/README.md
@@ -0,0 +1,3 @@
+# 2914. Minimum Number of Changes to Make Binary String Beautiful (medium)
+
+https://leetcode.com/problems/minimum-number-of-changes-to-make-binary-string-beautiful
---
+++ 2914. Minimum Number of Changes to Make Binary String Beautiful (medium)/main.cpp
@@ -0,0 +1,120 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+class Solution {
+public:
+  int minChanges(const std::string& s) {
+    const auto n = SZ(s);
+    std::string t;
+    FOR(i, 0, n-1) {
+      if (i == 0 || s[i-1] != s[i])
+        t += '1';
+      else
+        t.back() = (t.back() == '1') ? '0' : '1';
+    }
+    const auto m = SZ(t);
+    auto res = 0, j = -1;
+    FOR(i, 0, m-1) {
+      if (t[i] == '1') {
+        if (j == -1) {
+          j = i;
+        } else {
+          res += i - j;
+          j = -1;
+        }
+      }
+    }
+    return res;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().minChanges("1001");
+    assert(a_out == 2);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().minChanges("10");
+    assert(a_out == 1);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().minChanges("0000");
+    assert(a_out == 0);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
