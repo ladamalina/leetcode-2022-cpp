e5250948f0e35f50bc56867587651f5bea786a13
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-11-12 06:20:06 +0300

    3489. Zero Array Transformation IV (medium)

---
+++ 3489. Zero Array Transformation IV (medium)/README.md
@@ -0,0 +1,3 @@
+# 3489. Zero Array Transformation IV (medium)
+
+https://leetcode.com/problems/zero-array-transformation-iv
---
+++ 3489. Zero Array Transformation IV (medium)/main.cpp
@@ -0,0 +1,103 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vvl [[maybe_unused]] = std::vector<vl>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+class Solution {
+public:
+  int minZeroArray(vi& nums, const vvi& queries) {
+    const auto n = SZ(nums), qn = SZ(queries);
+    const auto max_x = *std::max_element(RNG(nums));
+    if (max_x == 0)
+      return 0;
+    vvi dp(n, vi(max_x + 1));
+    FOR(i, 0, n-1) {
+      dp[i][nums[i]] = 1;
+    }
+    FOR(qi, 0, qn-1) {
+      const auto val = queries[qi][2];
+      FOR(i, queries[qi][0], queries[qi][1]) {
+        FOR(j, 0, nums[i] - val) {
+          if (dp[i][j + val]) dp[i][j] = 1;
+        }
+      }
+      auto n_ok = 0;
+      FOR(i, 0, n-1) {
+        n_ok += dp[i][0];
+      }
+      if (n_ok == n)
+        return qi + 1;
+    }
+    return -1;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  /*{
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().solve();
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }*/
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
