700d4cf4d97b5a358c31ac6b6e95636d9016c0ff
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-11-12 09:50:59 +0300

    3011. Find if Array Can Be Sorted (medium)

---
+++ 3011. Find if Array Can Be Sorted (medium)/README.md
@@ -0,0 +1,3 @@
+# 3011. Find if Array Can Be Sorted (medium)
+
+https://leetcode.com/problems/find-if-array-can-be-sorted
---
+++ 3011. Find if Array Can Be Sorted (medium)/main.cpp
@@ -0,0 +1,93 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+class Solution {
+public:
+  bool canSortArray(const vi& nums) {
+    const auto n = SZ(nums);
+    vii mn_mx;
+    FOR(i, 0, n-1) {
+      if (i == 0 || __builtin_popcount(nums[i-1]) != __builtin_popcount(nums[i]))
+        mn_mx.emplace_back(nums[i], nums[i]);
+      mn_mx.back().F = std::min(mn_mx.back().F, nums[i]);
+      mn_mx.back().S = std::max(mn_mx.back().S, nums[i]);
+    }
+    const auto m = SZ(mn_mx);
+    FOR(i, 1, m-1) {
+      if (mn_mx[i-1].S > mn_mx[i].F)
+        return false;
+    }
+    return true;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  /*{
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().solve();
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }*/
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
