0c592075e6c75c167e4d70264eba446322e931f4
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-11-13 08:44:31 +0300

    2563. Count the Number of Fair Pairs (medium)

---
+++ 2563. Count the Number of Fair Pairs (medium)/README.md
@@ -0,0 +1,3 @@
+# 2563. Count the Number of Fair Pairs (medium)
+
+https://leetcode.com/problems/count-the-number-of-fair-pairs
---
+++ 2563. Count the Number of Fair Pairs (medium)/main.cpp
@@ -0,0 +1,103 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+class Solution {
+public:
+  ll countFairPairs(vi& nums, int lower, int upper) {
+    const auto n = SZ(nums);
+    SORT(nums);
+    ll res = 0;
+    FOR(i, 0, n-1) {
+      const auto it1 = std::lower_bound(RNG(nums), lower-nums[i]);
+      const auto j1 = CI(it1 - nums.begin());
+      const auto it2 = std::upper_bound(RNG(nums), upper-nums[i]);
+      const auto j2 = CI(it2 - nums.begin());
+      const auto i_res = std::max(j2, i+1) - std::max(j1, i+1);
+      res += i_res;
+    }
+    return res;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    vi nums{0,0,0,0,0,0};
+    const auto a_out = Solution().countFairPairs(nums, -1000000000, 1000000000);
+    assert(a_out == 15);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    vi nums{0,1,7,4,4,5};
+    const auto a_out = Solution().countFairPairs(nums, 3, 6);
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
