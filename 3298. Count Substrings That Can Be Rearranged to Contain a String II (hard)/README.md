740defbe4d983c301217ae3a3e0bc7d285d591ff
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-11-15 10:33:24 +0300

    3298. Count Substrings That Can Be Rearranged to Contain a String II (hard)

---
+++ 3298. Count Substrings That Can Be Rearranged to Contain a String II (hard)/README.md
@@ -0,0 +1,3 @@
+# 3298. Count Substrings That Can Be Rearranged to Contain a String II (hard)
+
+https://leetcode.com/problems/count-substrings-that-can-be-rearranged-to-contain-a-string-ii
---
+++ 3298. Count Substrings That Can Be Rearranged to Contain a String II (hard)/main.cpp
@@ -0,0 +1,145 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+using Freq = std::array<int, 26>;
+
+bool FreqLt(const Freq& a, const Freq& b) {
+  FOR(i, 0, 25) {
+    if (a[i] < b[i]) return true;
+  }
+  return false;
+}
+
+class Solution {
+public:
+  ll validSubstringCount(const std::string& s, const std::string& t) {
+    const auto sn = SZ(s), tn = SZ(t);
+    
+    Freq t_freq{};
+    FOR(i, 0, tn-1) ++t_freq[t[i]-'a'];
+
+    ll res = 0;
+    Freq s_freq{};
+    int l = 0, r = -1;
+    while (r+1 < sn && FreqLt(s_freq, t_freq)) {
+      ++s_freq[s[r+1]-'a'];
+      ++r;
+    }
+    if (!FreqLt(s_freq, t_freq))
+      res += sn - r;
+
+    while (l < sn) {
+      --s_freq[s[l]-'a'];
+      ++l;
+
+      while (r+1 < sn && FreqLt(s_freq, t_freq)) {
+        ++s_freq[s[r+1]-'a'];
+        ++r;
+      }
+      if (!FreqLt(s_freq, t_freq))
+        res += sn - r;
+    }
+    return res;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().validSubstringCount("bcca"s, "abc"s);
+    assert(a_out == 1);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().validSubstringCount("abcabc"s, "abc"s);
+    assert(a_out == 10);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().validSubstringCount("abcabc"s, "aaabc"s);
+    assert(a_out == 0);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().validSubstringCount("dddddededddeeeddd"s, "eee"s);
+    assert(a_out == 62);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
