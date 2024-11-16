9bd41505b34475ff99646492b977cd6fd1e50ce4
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-11-16 12:35:25 +0300

    3254. Find the Power of K-Size Subarrays I (medium)

---
+++ 3254. Find the Power of K-Size Subarrays I (medium)/README.md
@@ -0,0 +1,3 @@
+# 3254. Find the Power of K-Size Subarrays I (medium)
+
+https://leetcode.com/problems/find-the-power-of-k-size-subarrays-i
---
+++ 3254. Find the Power of K-Size Subarrays I (medium)/main.cpp
@@ -0,0 +1,142 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+constexpr int MAX_N = 500;
+std::array<int, MAX_N> diff{};
+
+class Solution {
+public:
+  vi resultsArray(const vi& nums, int k) {
+    const auto n = SZ(nums);
+    if (k == 1)
+      return nums;
+    // k >= 2
+
+    FOR(i, 1, n-1) {
+      diff[i] = nums[i] - nums[i-1];
+    }
+    
+    vi res;
+    auto cnt1 = 0;
+    FOR(r, 0, n-1) {
+      if (r >= k) {
+        const auto l = r - k + 1;
+        cnt1 += (diff[l] == 1 ? -1 : 0);
+      }
+      cnt1 += (r > 0 && diff[r] == 1);
+      if (r >= k-1) {
+        res.push_back(cnt1 == k-1 ? nums[r] : -1);
+      }
+    }
+    return res;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const vi nums{1,2,3,4,3,2,5};
+    const vi e_out{3,4,-1,-1,-1};
+    const auto a_out = Solution().resultsArray(nums, 3);
+    assert(a_out == e_out);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const vi nums{2,2,2,2,2};
+    const vi e_out{-1,-1};
+    const auto a_out = Solution().resultsArray(nums, 4);
+    assert(a_out == e_out);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const vi nums{3,2,3,2,3,2};
+    const vi e_out{-1,3,-1,3,-1};
+    const auto a_out = Solution().resultsArray(nums, 2);
+    assert(a_out == e_out);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const vi nums{1,3,4};
+    const vi e_out{-1,4};
+    const auto a_out = Solution().resultsArray(nums, 2);
+    assert(a_out == e_out);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
