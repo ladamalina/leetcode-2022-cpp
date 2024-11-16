e35547107ffa0172d0c50546993a7fb1d506d2f2
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-11-16 06:20:06 +0300

    3355. Zero Array Transformation I (medium)

---
+++ 3355. Zero Array Transformation I (medium)/README.md
@@ -0,0 +1,3 @@
+# 3355. Zero Array Transformation I (medium)
+
+https://leetcode.com/problems/zero-array-transformation-i
---
+++ 3355. Zero Array Transformation I (medium)/main.cpp
@@ -0,0 +1,96 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+class Solution {
+public:
+  bool isZeroArray(const vi& nums, const vvi& queries) {
+    const auto n = SZ(nums);
+    vi diff(n);
+    for (const auto& q : queries) {
+      const auto l = q[0], r = q[1];
+      ++diff[l];
+      if (r+1 < n)
+        --diff[r+1];
+    }
+    FOR(i, 1, n-1) {
+      diff[i] += diff[i-1];
+    }
+    bool ok = true;
+    FOR(i, 0, n-1) {
+      if (diff[i] < nums[i])
+        ok = false;
+    }
+    return ok;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  /*{
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().solve();
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }*/
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
