4d06623a183c4d73d900368c377e7a704aa021a2
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-11-16 12:35:39 +0300

    1574. Shortest Subarray to be Removed to Make Array Sorted (medium)

---
+++ 1574. Shortest Subarray to be Removed to Make Array Sorted (medium)/README.md
@@ -0,0 +1,3 @@
+# 1574. Shortest Subarray to be Removed to Make Array Sorted (medium)
+
+https://leetcode.com/problems/shortest-subarray-to-be-removed-to-make-array-sorted
---
+++ 1574. Shortest Subarray to be Removed to Make Array Sorted (medium)/main.cpp
@@ -0,0 +1,130 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+class Solution {
+public:
+  int findLengthOfShortestSubarray(const vi& arr) {
+    const auto n = SZ(arr);
+    auto l = 0, r = n-1;
+    FOR(i, 1, n-1) {
+      if (arr[i-1] <= arr[i])
+        ++l;
+      else break;
+    }
+    FORD(i, n-2, 0) {
+      if (arr[i] <= arr[i+1])
+        --r;
+      else break;
+    }
+    auto min_res = r;
+    FOR(i, 0, l) {
+      const auto ri = std::max(r, i+1);
+      const auto it = std::lower_bound(arr.begin()+ri, arr.end(), arr[i]);
+      const auto j = CI(it - arr.begin());
+      const auto res = j - i - 1;
+      min_res = std::min(min_res, res);
+    }
+    return min_res;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().findLengthOfShortestSubarray({1,2,3,10,4,2,3,5});
+    assert(a_out == 3);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().findLengthOfShortestSubarray({5,4,3,2,1});
+    assert(a_out == 4);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().findLengthOfShortestSubarray({1,2,3});
+    assert(a_out == 0);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().findLengthOfShortestSubarray({16,10,0,3,22,1,14,7,1,12,15});
+    assert(a_out == 8);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
