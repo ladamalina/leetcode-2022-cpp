5261442c1624b2ed9abbc8326cae19ab71fd2260
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-11-19 10:51:44 +0300

    2461. Maximum Sum of Distinct Subarrays With Length K (medium)

---
+++ 2461. Maximum Sum of Distinct Subarrays With Length K (medium)/README.md
@@ -0,0 +1,3 @@
+# 2461. Maximum Sum of Distinct Subarrays With Length K (medium)
+
+https://leetcode.com/problems/maximum-sum-of-distinct-subarrays-with-length-k
---
+++ 2461. Maximum Sum of Distinct Subarrays With Length K (medium)/main.cpp
@@ -0,0 +1,96 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+class Solution {
+public:
+  ll maximumSubarraySum(const vi& nums, int k) {
+    const auto n = SZ(nums);
+    std::unordered_map<int, int> x_freq;
+    ll sum = 0, max_sum = 0;
+    FOR(i, 0, n-1) {
+      if (i >= k) {
+        const auto l_val = nums[i - k];
+        --x_freq[l_val];
+        if (x_freq[l_val] == 0)
+          x_freq.erase(l_val);
+        sum -= l_val;
+      }
+      sum += nums[i];
+      if (SZ(x_freq) == k-1 && !x_freq.contains(nums[i]))
+        max_sum = std::max(max_sum, sum);
+      ++x_freq[nums[i]];
+    }
+    return max_sum;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  /*{
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().solve();
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }*/
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
