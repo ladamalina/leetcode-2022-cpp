11eed3bce539fda65969462a33bc8ea2cbfb4a51
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-11-26 06:20:06 +0300

    891. Sum of Subsequence Widths (hard)

---
+++ 891. Sum of Subsequence Widths (hard)/main.cpp
@@ -0,0 +1,113 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+constexpr ll MOD = 1e9 + 7;
+
+constexpr int MAX_N = 1e5;
+bool init_pow2 = false;
+vl pow2_mod(MAX_N+1);
+
+class Solution {
+public:
+  int sumSubseqWidths(vi& nums) {
+    if (!init_pow2) {
+      pow2_mod[0] = 1;
+      FOR(i, 1, MAX_N) {
+        pow2_mod[i] = (pow2_mod[i-1] * 2) % MOD;
+      }
+      init_pow2 = true;
+    }
+    
+    const auto n = SZ(nums);
+    SORT(nums);
+    ll res = 0;
+    FOR(i, 0, n-1) {
+      res = (res + ((nums[i] * pow2_mod[i]) % MOD)) % MOD;
+      res = (res - ((nums[i] * pow2_mod[n-i-1]) % MOD)) % MOD;
+    }
+    return res;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    vi nums{2,1,3};
+    const auto a_out = Solution().sumSubseqWidths(nums);
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    vi nums{2};
+    const auto a_out = Solution().sumSubseqWidths(nums);
+    assert(a_out == 0);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
