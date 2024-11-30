6b4d009c9523c1248e3ae10a6ad682fe1ee2c679
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-11-30 06:20:06 +0300

    316. Remove Duplicate Letters (medium)

---
+++ 316. Remove Duplicate Letters (medium)/README.md
@@ -0,0 +1,3 @@
+# 316. Remove Duplicate Letters (medium)
+
+https://leetcode.com/problems/remove-duplicate-letters
---
+++ 316. Remove Duplicate Letters (medium)/main.cpp
@@ -0,0 +1,124 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+class Solution {
+public:
+  std::string removeDuplicateLetters(const std::string& s) {
+    const auto n = SZ(s);
+    vvi ci_ids(26);
+    FOR(i, 0, n-1) {
+      ci_ids[s[i]-'a'].push_back(i);
+    }
+    FORD(ci, 25, 0) {
+      std::reverse(RNG(ci_ids[ci]));
+    }
+    
+    std::string t;
+    vi used(26);
+    for (const auto c : s) {
+      if (used[c-'a']) {
+        ci_ids[c-'a'].pop_back();
+        continue;
+      }
+      while (!t.empty() && t.back() > c && !ci_ids[t.back()-'a'].empty()) {
+        used[t.back()-'a'] = false;
+        t.pop_back();
+      }
+      t += c;
+      used[c-'a'] = true;
+      ci_ids[c-'a'].pop_back();
+    }
+    return t;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const auto a_out = Solution().removeDuplicateLetters("bcabc");
+    assert(a_out == "abc");
+
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().removeDuplicateLetters("cbacdcbc");
+    assert(a_out == "acdb");
+
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const auto a_out = Solution().removeDuplicateLetters("bbcaac");
+    assert(a_out == "bac");
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
