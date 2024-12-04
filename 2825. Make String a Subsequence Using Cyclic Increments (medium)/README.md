9018f0ff9a0381d6d848902f0ff795361d2ef84b
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-12-04 04:27:28 +0300

    2825. Make String a Subsequence Using Cyclic Increments (medium)

---
+++ 2825. Make String a Subsequence Using Cyclic Increments (medium)/README.md
@@ -0,0 +1,3 @@
+# 2825. Make String a Subsequence Using Cyclic Increments (medium)
+
+https://leetcode.com/problems/make-string-a-subsequence-using-cyclic-increments
---
+++ 2825. Make String a Subsequence Using Cyclic Increments (medium)/main.cpp
@@ -0,0 +1,95 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+class Solution {
+public:
+  bool canMakeSubsequence(const std::string& str1, const std::string& str2) {
+    const auto n = SZ(str1), m = SZ(str2);
+    auto i1 = 0, i2 = 0;
+    while (i1 < n && i2 < m) {
+      if (str1[i1] == str2[i2] || Next(str1[i1]) == str2[i2]) {
+        ++i1;
+        ++i2;
+      } else {
+        ++i1;
+      }
+    }
+    return i2 == m;
+  }
+
+private:
+  static char Next(const char c) {
+    return (c == 'z') ? 'a' : static_cast<char>(c + 1);
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  /*{
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().solve();
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }*/
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
