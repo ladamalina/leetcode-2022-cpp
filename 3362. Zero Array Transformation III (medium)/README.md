65e3958cb064787a61d69b55b8e6c3d2be5feb68
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-12-05 11:20:26 +0300

    3362. Zero Array Transformation III (medium)

---
+++ 3362. Zero Array Transformation III (medium)/README.md
@@ -0,0 +1,3 @@
+# 3362. Zero Array Transformation III (medium)
+
+https://leetcode.com/problems/zero-array-transformation-iii
---
+++ 3362. Zero Array Transformation III (medium)/main.cpp
@@ -0,0 +1,103 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+class Solution {
+public:
+  int maxRemoval(vi& nums, vvi& queries) {
+    const auto n = SZ(nums), m = SZ(queries);
+    SORT(queries);
+
+    std::priority_queue<int, vi, std::less<>> opened;
+    std::priority_queue<int, vi, std::greater<>> taken;
+    auto n_taken = 0, qi = 0;
+    FOR(i, 0, n-1) {
+      while (qi < m && queries[qi][0] == i) {
+        opened.push(queries[qi][1]);
+        ++qi;
+      }
+      nums[i] -= SZ(taken);
+      while (nums[i] > 0 && !opened.empty() && opened.top() >= i) {
+        ++n_taken;
+        taken.push(opened.top());
+        opened.pop();
+        --nums[i];
+      } 
+      if (nums[i] > 0)
+        return -1;
+      while (!taken.empty() && taken.top() == i)
+        taken.pop();
+    }
+    return m - n_taken;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  /*{
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().solve();
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }*/
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
