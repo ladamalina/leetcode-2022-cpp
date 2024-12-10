f41a6a85534cd9c9db8ef8813ab9c19bdde3d1bc
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-12-10 10:06:39 +0300

    3361. Shift Distance Between Two Strings (medium)

---
+++ 3361. Shift Distance Between Two Strings (medium)/README.md
@@ -0,0 +1,3 @@
+# 3361. Shift Distance Between Two Strings (medium)
+
+https://leetcode.com/problems/shift-distance-between-two-strings
---
+++ 3361. Shift Distance Between Two Strings (medium)/main.cpp
@@ -0,0 +1,113 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+class Solution {
+public:
+  ll shiftDistance(const std::string& s, const std::string& t, const vi& n_cost, const vi& p_cost) {
+    const auto n = SZ(s);
+    ll res = 0;
+    FOR(i, 0, n-1) {
+      ll n_res = 0;
+      auto nc = s[i];
+      while (nc != t[i]) {
+        n_res += n_cost[nc-'a'];
+        nc = (nc == 'z') ? 'a' : static_cast<char>(nc + 1);
+      }
+      
+      ll p_res = 0;
+      auto pc = s[i];
+      while (pc != t[i]) {
+        p_res += p_cost[pc-'a'];
+        pc = (pc == 'a') ? 'z' : static_cast<char>(pc - 1);
+      }
+      
+      res += std::min(n_res, p_res);
+    }
+    return res;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const vi n_cost{100,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    const vi p_cost{1,100,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    const auto a_out = Solution().shiftDistance("abab"s, "baba"s, n_cost, p_cost);
+    assert(a_out == 2);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const vi n_cost{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+    const vi p_cost{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+    const auto a_out = Solution().shiftDistance("leet"s, "code"s, n_cost, p_cost);
+    assert(a_out == 31);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
