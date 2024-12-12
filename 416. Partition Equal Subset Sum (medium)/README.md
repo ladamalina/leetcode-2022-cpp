7609e24b2f2173a09964270f1b694c58c9ce70ba
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-12-12 13:29:43 +0300

    416. Partition Equal Subset Sum (medium)

---
+++ 416. Partition Equal Subset Sum (medium)/README.md
@@ -0,0 +1,3 @@
+# 416. Partition Equal Subset Sum (medium)
+
+https://leetcode.com/problems/partition-equal-subset-sum
---
+++ 416. Partition Equal Subset Sum (medium)/main.cpp
@@ -0,0 +1,112 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+class Solution {
+public:
+  bool canPartition(const vi& nums) {
+    const auto all_sum = std::accumulate(RNG(nums), 0);
+    if (all_sum % 2 == 1)
+      return false;
+    const auto target_sum = all_sum / 2;
+
+    const auto n = SZ(nums);
+    vvi dp(n, vi(target_sum+1, -1));
+    
+    const std::function<bool(int,int)> rec = [&](const int i, const int l_sum) {
+      if (i == n)
+        return l_sum == target_sum;
+      if (l_sum > target_sum)
+        return false;
+      if (dp[i][l_sum] != -1)
+        return dp[i][l_sum] == 1;
+
+      const auto res = rec(i+1, l_sum + nums[i]) || rec(i+1, l_sum);
+      dp[i][l_sum] = res;
+
+      return res;
+    };
+    const auto res = rec(0, 0);
+    return res;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().canPartition({1,5,11,5});
+    assert(a_out);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().canPartition({1,2,3,5});
+    assert(!a_out);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
