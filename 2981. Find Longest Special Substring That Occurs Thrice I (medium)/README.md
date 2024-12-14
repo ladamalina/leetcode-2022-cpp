b8de2571910389e37b9b1e9b303d37e397d376e9
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2024-12-14 05:32:14 +0300

    2981. Find Longest Special Substring That Occurs Thrice I (medium)

---
+++ 2981. Find Longest Special Substring That Occurs Thrice I (medium)/README.md
@@ -0,0 +1,3 @@
+# 2981. Find Longest Special Substring That Occurs Thrice I (medium)
+
+https://leetcode.com/problems/find-longest-special-substring-that-occurs-thrice-i
---
+++ 2981. Find Longest Special Substring That Occurs Thrice I (medium)/main.cpp
@@ -0,0 +1,118 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+class Solution {
+public:
+  int maximumLength(const std::string& s) {
+    const auto n = SZ(s);
+    int max_len = 0;
+    FOR(i, 0, n-1) {
+      FOR(i2, i, n-1) {
+        if (s[i2] != s[i])
+          break;
+        // s[i]..s[i2] is aaaa
+        const auto t = s.substr(i, i2-i+1);
+        FOR(j, i+1, n-1) {
+          if (!s.substr(j).starts_with(t))
+            continue;
+          FOR(k, j+1, n-1) {
+            if (s.substr(k).starts_with(t))
+              max_len = std::max(max_len, SZ(t));
+          }
+        }
+      }
+    }
+    return max_len ? max_len : -1;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().maximumLength("aaaa");
+    assert(a_out == 2);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().maximumLength("abcdef");
+    assert(a_out == -1);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().maximumLength("abcaba");
+    assert(a_out == 1);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
