ad01b3c21f8a7731a5cdee294fdb1372faa90397
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2025-02-18 06:20:06 +0300

    2698. Find the Punishment Number of an Integer (medium)

---
+++ 2698. Find the Punishment Number of an Integer (medium)/README.md
@@ -0,0 +1,3 @@
+# 2698. Find the Punishment Number of an Integer (medium)
+
+https://leetcode.com/problems/find-the-punishment-number-of-an-integer
---
+++ 2698. Find the Punishment Number of an Integer (medium)/main.cpp
@@ -0,0 +1,105 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vvl [[maybe_unused]] = std::vector<vl>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+class Solution {
+public:
+  bool Rec(int i, int n, const std::string& s, int target, int psum) {
+    if (i == n)
+      return psum == target;
+    FOR(j, i, n-1) {
+      const auto add = std::stoi(s.substr(i, j-i+1));
+      if (psum + add <= target) {
+        if (Rec(j+1, n, s, target, psum + add))
+          return true;
+      } else {
+        break;
+      }
+    }
+    return false;
+  }
+  
+  bool Check(const int x) {
+    const auto s = std::to_string(x*x);
+    return Rec(0, SZ(s), s, x, 0);
+  }
+
+  int punishmentNumber(const int n) {
+    int res = 0;
+    FOR(x, 1, n) {
+      res += Check(x) ? x*x : 0;
+    }
+    return res;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  /*{
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().solve();
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }*/
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
