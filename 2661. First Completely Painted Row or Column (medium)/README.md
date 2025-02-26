30c4182ad098d5a924a3344a6cf89684774fd777
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2025-02-26 06:20:06 +0300

    2661. First Completely Painted Row or Column (medium)

---
+++ 2661. First Completely Painted Row or Column (medium)/README.md
@@ -0,0 +1,3 @@
+# 2661. First Completely Painted Row or Column (medium)
+
+https://leetcode.com/problems/first-completely-painted-row-or-column
---
+++ 2661. First Completely Painted Row or Column (medium)/main.cpp
@@ -0,0 +1,99 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vvl [[maybe_unused]] = std::vector<vl>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+constexpr int MAX_N = 1e5;
+std::array<ii, MAX_N> pos;
+std::array<int, MAX_N> rows, cols;
+
+class Solution {
+public:
+  int firstCompleteIndex(const vi& arr, const vvi& mat) {
+    const auto n = SZ(mat), m = SZ(mat[0]);
+    vii pos(n*m);
+    FOR(i, 0, n-1) {
+      FOR(j, 0, m-1) {
+        pos[mat[i][j]-1] = {i, j};
+      }
+    }
+    std::fill(rows.begin(), rows.begin()+n, 0);
+    std::fill(cols.begin(), cols.begin()+m, 0);
+    FOR(i, 0, n*m-1) {
+      const auto [ci, cj] = pos[arr[i]-1];
+      if (++rows[ci] == m || ++cols[cj] == n)
+        return i;
+    }
+    return -1;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  /*{
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().solve();
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }*/
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
