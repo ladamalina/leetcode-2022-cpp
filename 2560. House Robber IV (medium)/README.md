064ed3a6d0e694af92d21d9da7ab2f02377e4a9f
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2025-03-19 06:20:06 +0300

    2560. House Robber IV (medium)

---
+++ 2560. House Robber IV (medium)/README.md
@@ -0,0 +1,3 @@
+# 2560. House Robber IV (medium)
+
+https://leetcode.com/problems/house-robber-iv
---
+++ 2560. House Robber IV (medium)/main.cpp
@@ -0,0 +1,103 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vvl [[maybe_unused]] = std::vector<vl>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+bool BinCheck(const int lim, const vi& nums, const int k) {
+  const auto n = SZ(nums);
+  auto res = 0, len = 0;
+  FOR(i, 0, n-1) {
+    if (nums[i] > lim) continue;
+    ++len;
+    if (i == n-1 || nums[i+1] > lim) {
+      res += (len + 1) / 2;
+      len = 0;
+    }
+  }
+  return res >= k;
+}
+
+class Solution {
+public:
+  int minCapability(const vi& nums, const int k) {
+    int lo = 1, hi = *std::max_element(RNG(nums));
+    while (lo < hi) {
+      const auto mid = (lo + hi) / 2;
+      if (BinCheck(mid, nums, k))
+        hi = mid;
+      else
+        lo = mid + 1;
+    }
+    return lo;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  /*{
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().solve();
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }*/
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
