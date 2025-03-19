c77b51ed646b69ea19dd03d983eb8b8b00d24402
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2025-03-19 06:20:06 +0300

    2559. Count Vowel Strings in Ranges (medium)

---
+++ 2559. Count Vowel Strings in Ranges (medium)/README.md
@@ -0,0 +1,3 @@
+# 2559. Count Vowel Strings in Ranges (medium)
+
+https://leetcode.com/problems/count-vowel-strings-in-ranges
---
+++ 2559. Count Vowel Strings in Ranges (medium)/main.cpp
@@ -0,0 +1,97 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ii [[maybe_unused]] = std::pair<int, int>;
+using vi [[maybe_unused]] = std::vector<int>;
+using vl [[maybe_unused]] = std::vector<ll>;
+using vvi [[maybe_unused]] = std::vector<vi>;
+using vvl [[maybe_unused]] = std::vector<vl>;
+using vii [[maybe_unused]] = std::vector<ii>;
+using vb [[maybe_unused]] = std::vector<bool>;
+using vd [[maybe_unused]] = std::vector<double>;
+using vs [[maybe_unused]] = std::vector<std::string>;
+
+#define FOR(_i, _a, _b) for (int _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (int _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<double>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+bool Check(const std::string& s) {
+  const auto n = SZ(s);
+  return (s[0]=='a'||s[0]=='e'||s[0]=='i'||s[0]=='o'||s[0]=='u')
+    && (s[n-1]=='a'||s[n-1]=='e'||s[n-1]=='i'||s[n-1]=='o'||s[n-1]=='u');
+}
+
+class Solution {
+public:
+  vi vowelStrings(const vs& words, const vvi& queries) {
+    const auto n = SZ(words), qn = SZ(queries);
+    vi psum(n);
+    FOR(i, 0, n-1) {
+      psum[i] = Check(words[i]) + (i > 0 ? psum[i-1] : 0);
+    }
+    vi ans(qn);
+    FOR(qi, 0, qn-1) {
+      const auto l = queries[qi][0], r = queries[qi][1];
+      ans[qi] = psum[r] - (l > 0 ? psum[l-1] : 0);
+    }
+    return ans;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  /*{
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().solve();
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }*/
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
