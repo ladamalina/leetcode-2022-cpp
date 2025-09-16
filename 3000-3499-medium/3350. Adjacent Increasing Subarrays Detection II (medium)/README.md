5938b5367a84e8da4502e28fe79995d338d91063
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2025-09-16 06:20:06 +0300

    3350. Adjacent Increasing Subarrays Detection II (medium)

---
+++ 3000-3499-medium/3350. Adjacent Increasing Subarrays Detection II (medium)/README.md
@@ -0,0 +1,3 @@
+# 3350. Adjacent Increasing Subarrays Detection II (medium)
+
+https://leetcode.com/problems/adjacent-increasing-subarrays-detection-ii/
---
+++ 3000-3499-medium/3350. Adjacent Increasing Subarrays Detection II (medium)/main.cpp
@@ -0,0 +1,101 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ld = long double;
+using ii = std::pair<int, int>;
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vvvi = std::vector<vvi>;
+using vl = std::vector<ll>;
+using vvl = std::vector<vl>;
+using vvvl = std::vector<vvl>;
+using vii = std::vector<ii>;
+using vb = std::vector<bool>;
+using vd = std::vector<ld>;
+using vs = std::vector<std::string>;
+using vc = std::vector<char>;
+
+#define FOR(_i, _a, _b) for (auto _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (auto _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<ld>(_v)
+#define CC(_v) static_cast<char>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+#define PB push_back
+
+constexpr int MAX_N = 2 * 1e5;
+std::array<int, MAX_N> r_arr;
+
+class Solution {
+public:
+  int maxIncreasingSubarrays(const vi& nums) {
+    const auto n = SZ(nums);
+
+    r_arr[n-1] = 1;
+    FORD(i, n-2, 0) r_arr[i] = nums[i] < nums[i+1] ? r_arr[i+1] + 1 : 1;
+
+    auto max_res = 0, l_arr = 1;
+    FOR(i, 1, n-1) {
+      max_res = std::max(max_res, std::min(l_arr, r_arr[i]));
+      l_arr = nums[i-1] < nums[i] ? l_arr + 1 : 1;
+    }
+
+    return max_res;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  /*{
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().solve();
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }*/
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
