024ae899169902cb19ca76dd823b8d6ae9f1416a
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2025-09-25 06:20:06 +0300

    3147. Taking Maximum Energy From the Mystic Dungeon (medium)

---
+++ 3000-3499-medium/3147. Taking Maximum Energy From the Mystic Dungeon (medium)/README.md
@@ -0,0 +1,3 @@
+# 3147. Taking Maximum Energy From the Mystic Dungeon (medium)
+
+https://leetcode.com/problems/taking-maximum-energy-from-the-mystic-dungeon/
---
+++ 3000-3499-medium/3147. Taking Maximum Energy From the Mystic Dungeon (medium)/main.cpp
@@ -0,0 +1,96 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ld = long double;
+using ii = std::pair<int, int>;
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vvvi = std::vector<vvi>;
+using vl = std::vector<ll>;
+using vvl = std::vector<vl>;
+using vvvl = std::vector<vvl>;
+using vii = std::vector<ii>;
+using vb = std::vector<bool>;
+using vd = std::vector<ld>;
+using vs = std::vector<std::string>;
+using vc = std::vector<char>;
+
+#define FOR(_i, _a, _b) for (auto _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (auto _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<ld>(_v)
+#define CC(_v) static_cast<char>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+#define PB push_back
+
+constexpr int MAX_N = 1e5;
+std::array<int, MAX_N> arr;
+
+class Solution {
+public:
+  int maximumEnergy(const vi& energy, const int k) {
+    const auto n = SZ(energy);
+    auto max_res = INT_MIN;
+    FORD(i, n-1, 0) {
+      arr[i] = energy[i] + (i+k < n ? arr[i+k] : 0);
+      max_res = std::max(max_res, arr[i]);
+    }
+    return max_res;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  /*{
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().solve();
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }*/
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
