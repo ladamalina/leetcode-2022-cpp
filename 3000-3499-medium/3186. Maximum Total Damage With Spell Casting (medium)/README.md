5eeb23484d71f2775377091757857d73ad543cd4
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2025-09-25 06:20:06 +0300

    3186. Maximum Total Damage With Spell Casting (medium)

---
+++ 3000-3499-medium/3186. Maximum Total Damage With Spell Casting (medium)/README.md
@@ -0,0 +1,3 @@
+# 3186. Maximum Total Damage With Spell Casting (medium)
+
+https://leetcode.com/problems/maximum-total-damage-with-spell-casting/
---
+++ 3000-3499-medium/3186. Maximum Total Damage With Spell Casting (medium)/main.cpp
@@ -0,0 +1,100 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ld = long double;
+using ii = std::pair<int, int>;
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vvvi = std::vector<vvi>;
+using vl = std::vector<ll>;
+using vvl = std::vector<vl>;
+using vvvl = std::vector<vvl>;
+using vii = std::vector<ii>;
+using vb = std::vector<bool>;
+using vd = std::vector<ld>;
+using vs = std::vector<std::string>;
+using vc = std::vector<char>;
+
+#define FOR(_i, _a, _b) for (auto _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (auto _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<ld>(_v)
+#define CC(_v) static_cast<char>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+#define PB push_back
+
+constexpr int MAX_N = 1e5;
+std::array<ll, MAX_N + 1> dp;
+
+class Solution {
+public:
+  ll maximumTotalDamage(vi& power) {
+    const auto n = SZ(power);
+    SORT(power);
+    dp[n] = 0;
+
+    FORD(i, n-1, 0) {
+      const auto j = CI(std::upper_bound(power.begin() + i, power.end(), power[i]) - power.begin());
+      const auto k = CI(std::upper_bound(power.begin() + j, power.end(), power[i] + 2) - power.begin());
+      dp[i] = std::max(dp[i+1], CL(power[i]) * (j - i) + dp[k]);
+    }
+
+    return dp[0];
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  /*{
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().solve();
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }*/
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
