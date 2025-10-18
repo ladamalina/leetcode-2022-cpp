d3825cdeee56fcabd3b7c8b98de575c5737613f2
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2025-10-18 06:20:06 +0300

    3733. Minimum Time to Complete All Deliveries (medium)

---
+++ 3500-3999-medium/3733. Minimum Time to Complete All Deliveries (medium)/README.md
@@ -0,0 +1,3 @@
+# 3733. Minimum Time to Complete All Deliveries (medium)
+
+https://leetcode.com/problems/minimum-time-to-complete-all-deliveries/
---
+++ 3500-3999-medium/3733. Minimum Time to Complete All Deliveries (medium)/main.cpp
@@ -0,0 +1,174 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ld = long double;
+using ii = std::pair<int, int>;
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vvvi = std::vector<vvi>;
+using vl = std::vector<ll>;
+using vvl = std::vector<vl>;
+using vvvl = std::vector<vvl>;
+using vii = std::vector<ii>;
+using vb = std::vector<bool>;
+using vd = std::vector<ld>;
+using vs = std::vector<std::string>;
+using vc = std::vector<char>;
+
+#define FOR(_i, _a, _b) for (auto _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (auto _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<ld>(_v)
+#define CC(_v) static_cast<char>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+#define PB push_back
+
+template <typename T>
+T LCM(T a, T b) {
+  if (a > b) return (a / std::gcd(a, b)) * b;
+  return (b / std::gcd(a, b)) * a;
+}
+
+class Solution {
+public:
+  ll minimumTime(vi& d, vi& r) {
+    const auto lcm = LCM(r[0], r[1]);
+    
+    const std::function<bool(ll)> bin_check = [&](const ll t) {
+      const auto idle_both = t / lcm;
+      const auto idle_0 = t / r[0], idle_1 = t / r[1];
+      const auto idle_any = idle_0 + idle_1 - idle_both;
+      const auto b0 = idle_1 - idle_both, b1 = idle_0 - idle_both;
+      const auto c0 = std::max(0ll, d[0] - b0), c1 = std::max(0ll, d[1] - b1);
+      return t - idle_any >= c0 + c1;
+    };
+    
+    ll lo = 2, hi = 2ll * (d[0] + d[1]);
+    while (lo < hi) {
+      const auto mid = (lo + hi) >> 1;
+      const auto check = bin_check(mid);
+      // std::cerr << lo << '\t' << mid << '\t' << hi << '\t' << check << std::endl;
+      if (check) hi = mid;
+      else lo = mid + 1;
+    }
+    return lo;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    vi d{2,2};
+    vi r{3,4};
+    const auto a_out = Solution().minimumTime(d, r);
+    assert(a_out == 4);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    vi d{1,1};
+    vi r{3,2};
+    const auto a_out = Solution().minimumTime(d, r);
+    assert(a_out == 2);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    vi d{3,1};
+    vi r{2,3};
+    const auto a_out = Solution().minimumTime(d, r);
+    assert(a_out == 5);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    vi d{1,3};
+    vi r{2,2};
+    const auto a_out = Solution().minimumTime(d, r);
+    assert(a_out == 7);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    vi d{2,1};
+    vi r{3,4};
+    const auto a_out = Solution().minimumTime(d, r);
+    assert(a_out == 3);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    vi d{1,2};
+    vi r{2,3};
+    const auto a_out = Solution().minimumTime(d, r);
+    assert(a_out == 3);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
