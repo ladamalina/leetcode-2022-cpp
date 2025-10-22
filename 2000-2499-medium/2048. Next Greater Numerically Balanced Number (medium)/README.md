06914379f6a97a8a239d1819d86f3b41ddbc71f4
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2025-10-22 06:20:06 +0300

    2048. Next Greater Numerically Balanced Number (medium)

---
+++ 2000-2499-medium/2048. Next Greater Numerically Balanced Number (medium)/README.md
@@ -0,0 +1,3 @@
+# 2048. Next Greater Numerically Balanced Number (medium)
+
+https://leetcode.com/problems/next-greater-numerically-balanced-number/
---
+++ 2000-2499-medium/2048. Next Greater Numerically Balanced Number (medium)/main.cpp
@@ -0,0 +1,129 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ld = long double;
+using ii = std::pair<int, int>;
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vvvi = std::vector<vvi>;
+using vl = std::vector<ll>;
+using vvl = std::vector<vl>;
+using vvvl = std::vector<vvl>;
+using vii = std::vector<ii>;
+using vb = std::vector<bool>;
+using vd = std::vector<ld>;
+using vs = std::vector<std::string>;
+using vc = std::vector<char>;
+
+#define FOR(_i, _a, _b) for (auto _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (auto _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<ld>(_v)
+#define CC(_v) static_cast<char>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+#define PB push_back
+
+vi nums;
+bool init_nums = false;
+
+void Rec(const int dn, const int max_d, std::string& s) {
+  if (dn == 0) {
+    std::string t;
+    for (const auto c : s) t += std::string(c - '0', c);
+    SORT(t);
+    do {
+      nums.push_back(std::stoi(t));
+    } while (std::next_permutation(RNG(t)));
+    return;
+  } // dn > 0
+  if (max_d == 0) return;
+  FOR(d, 1, std::min(max_d, dn)) {
+    s.push_back(CC('0' + d));
+    Rec(dn - d, d - 1, s);
+    s.pop_back();
+  }
+}
+
+void InitNums() {
+  if (init_nums) return;
+  FOR(dn, 1, 7) {
+    std::string s;
+    Rec(dn, dn, s);
+  }
+  SORT(nums);
+  init_nums = true;
+}
+
+class Solution {
+public:
+  int nextBeautifulNumber(const int n) {
+    InitNums();
+    return *std::upper_bound(RNG(nums), n);
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().nextBeautifulNumber(1);
+    assert(a_out == 22);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().nextBeautifulNumber(1000);
+    assert(a_out == 1333);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
