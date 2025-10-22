731a15530b97251bf2eed7c6936fc66caa786acf
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2025-10-22 06:20:06 +0300

    2043. Simple Bank System (medium)

---
+++ 2000-2499-medium/2043. Simple Bank System (medium)/README.md
@@ -0,0 +1,3 @@
+# 2043. Simple Bank System (medium)
+
+https://leetcode.com/problems/simple-bank-system/
---
+++ 2000-2499-medium/2043. Simple Bank System (medium)/main.cpp
@@ -0,0 +1,122 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ld = long double;
+using ii = std::pair<int, int>;
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vvvi = std::vector<vvi>;
+using vl = std::vector<ll>;
+using vvl = std::vector<vl>;
+using vvvl = std::vector<vvl>;
+using vii = std::vector<ii>;
+using vb = std::vector<bool>;
+using vd = std::vector<ld>;
+using vs = std::vector<std::string>;
+using vc = std::vector<char>;
+
+#define FOR(_i, _a, _b) for (auto _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (auto _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<ld>(_v)
+#define CC(_v) static_cast<char>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+#define PB push_back
+
+class Bank {
+public:
+  explicit Bank(std::vector<long long>& balance) : n_(balance.size()), b_(balance) {}
+    
+  bool transfer(int account1, int account2, long long money) {
+    if (account1 <= n_ && account2 <= n_ && money <= b_[account1 - 1]) {
+      b_[account1 - 1] -= money;
+      b_[account2 - 1] += money;
+      return true;
+    }
+    return false;
+  }
+    
+  bool deposit(int account, long long money) {
+    if (account <= n_) {
+      b_[account - 1] += money;
+      return true;
+    }
+    return false;
+  }
+    
+  bool withdraw(int account, long long money) {
+    if (account <= n_ && money <= b_[account - 1]) {
+      b_[account - 1] -= money;
+      return true;
+    }
+    return false;
+  }
+
+private:
+  const int n_;
+  std::vector<long long>& b_;
+};
+
+/**
+ * Your Bank object will be instantiated and called as such:
+ * Bank* obj = new Bank(balance);
+ * bool param_1 = obj->transfer(account1,account2,money);
+ * bool param_2 = obj->deposit(account,money);
+ * bool param_3 = obj->withdraw(account,money);
+ */
+
+[[maybe_unused]] void TestSolution() {
+  /*{
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().solve();
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }*/
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
