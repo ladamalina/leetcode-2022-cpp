b157189eb47df06675a86baedec6b12fc9c6e54d
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2025-11-09 03:07:41 +0300

    1578. Minimum Time to Make Rope Colorful (medium)

---
+++ 1500-1999-medium/1578. Minimum Time to Make Rope Colorful (medium)/README.md
@@ -0,0 +1,3 @@
+# 1578. Minimum Time to Make Rope Colorful (medium)
+
+https://leetcode.com/problems/minimum-time-to-make-rope-colorful/
---
+++ 1500-1999-medium/1578. Minimum Time to Make Rope Colorful (medium)/main.cpp
@@ -0,0 +1,102 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ld = long double;
+using ii = std::pair<int, int>;
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vvvi = std::vector<vvi>;
+using vl = std::vector<ll>;
+using vvl = std::vector<vl>;
+using vvvl = std::vector<vvl>;
+using vii = std::vector<ii>;
+using vb = std::vector<bool>;
+using vd = std::vector<ld>;
+using vs = std::vector<std::string>;
+using vc = std::vector<char>;
+
+#define FOR(_i, _a, _b) for (auto _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (auto _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<ld>(_v)
+#define CC(_v) static_cast<char>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+#define PB push_back
+
+class Solution {
+public:
+  int minCost(const std::string& cs, const vi& ts) {
+    const auto n = SZ(cs);
+    auto res = 0, t_max = ts[0], t_sum = ts[0];
+    FOR(i, 1, n-1) {
+      if (cs[i-1] == cs[i]) {
+        t_max = std::max(t_max, ts[i]);
+        t_sum += ts[i];
+      } else {
+        res += t_sum - t_max;
+        t_max = ts[i];
+        t_sum = ts[i];
+      }
+      if (i == n-1) res += t_sum - t_max;
+    }
+    return res;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const auto cs = "bbbaaa"s;
+    const vi ts{4,9,3,8,8,9};
+    const auto a_out = Solution().minCost(cs, ts);
+    assert(a_out == 23);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
