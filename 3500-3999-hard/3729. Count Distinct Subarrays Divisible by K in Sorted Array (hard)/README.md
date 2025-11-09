c52822d6413bb30bcf606720208d4a8472707757
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2025-11-09 03:06:52 +0300

    3729. Count Distinct Subarrays Divisible by K in Sorted Array (hard)

---
+++ 3500-3999-hard/3729. Count Distinct Subarrays Divisible by K in Sorted Array (hard)/README.md
@@ -0,0 +1,3 @@
+# 3729. Count Distinct Subarrays Divisible by K in Sorted Array (hard)
+
+https://leetcode.com/problems/count-distinct-subarrays-divisible-by-k-in-sorted-array/
---
+++ 3500-3999-hard/3729. Count Distinct Subarrays Divisible by K in Sorted Array (hard)/main.cpp
@@ -0,0 +1,138 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ld = long double;
+using ii = std::pair<int, int>;
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vvvi = std::vector<vvi>;
+using vl = std::vector<ll>;
+using vvl = std::vector<vl>;
+using vvvl = std::vector<vvl>;
+using vii = std::vector<ii>;
+using vb = std::vector<bool>;
+using vd = std::vector<ld>;
+using vs = std::vector<std::string>;
+using vc = std::vector<char>;
+
+#define FOR(_i, _a, _b) for (auto _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (auto _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<ld>(_v)
+#define CC(_v) static_cast<char>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+#define PB push_back
+
+class Solution {
+public:
+  ll numGoodSubarrays(const vi& nums, const int k) {
+    const auto n = SZ(nums);
+    
+    std::unordered_map<int, int> x_max_id;
+    FOR(i, 0, n-1) x_max_id[nums[i]] = i;
+    
+    std::unordered_map<int, vi> mod_ids;
+    {
+      ll psum = 0;
+      FOR(i, 0, n-1) {
+        psum += nums[i];
+        mod_ids[psum % k].PB(i);
+      }
+    }
+
+    ll res = 0, psum = 0;
+    FOR(i, 0, n-1) {
+      const auto it = mod_ids.find(psum % k);
+      if (it != mod_ids.end()) {
+        
+        const auto it2 = (i == 0 || nums[i-1] != nums[i])
+          ? std::lower_bound(RNG(it->S), i)
+          : std::upper_bound(RNG(it->S), x_max_id[nums[i]]);
+        const auto cnt = CI(it->S.end() - it2);
+        res += cnt;
+      }
+      psum += nums[i];
+    }
+
+    return res;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const vi nums{1,1,2};
+    const auto a_out = Solution().numGoodSubarrays(nums, 1);
+    assert(a_out == 5);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const vi nums{1,2,3};
+    const auto a_out = Solution().numGoodSubarrays(nums, 3);
+    assert(a_out == 3);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const vi nums{2,2,2,2,2,2};
+    const auto a_out = Solution().numGoodSubarrays(nums, 6);
+    assert(a_out == 2);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
