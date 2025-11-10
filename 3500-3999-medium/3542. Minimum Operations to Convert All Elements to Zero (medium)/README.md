49ae60d97b11da522bd121397094f650f0c17583
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2025-11-10 03:41:07 +0300

    3542. Minimum Operations to Convert All Elements to Zero (medium)

---
+++ 3500-3999-medium/3542. Minimum Operations to Convert All Elements to Zero (medium)/README.md
@@ -0,0 +1,3 @@
+# 3542. Minimum Operations to Convert All Elements to Zero (medium)
+
+https://leetcode.com/problems/minimum-operations-to-convert-all-elements-to-zero/
---
+++ 3500-3999-medium/3542. Minimum Operations to Convert All Elements to Zero (medium)/main.cpp
@@ -0,0 +1,139 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ld = long double;
+using ii = std::pair<int, int>;
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vvvi = std::vector<vvi>;
+using vl = std::vector<ll>;
+using vvl = std::vector<vl>;
+using vvvl = std::vector<vvl>;
+using vii = std::vector<ii>;
+using vb = std::vector<bool>;
+using vd = std::vector<ld>;
+using vs = std::vector<std::string>;
+using vc = std::vector<char>;
+
+#define FOR(_i, _a, _b) for (auto _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (auto _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<ld>(_v)
+#define CC(_v) static_cast<char>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+#define PB push_back
+
+class Solution {
+public:
+  int minOperations(const vi& nums) {
+    const auto n = SZ(nums);
+    std::map<int, vi> x_ids;
+    FOR(i, 0, n-1) x_ids[nums[i]].PB(i);
+    std::set<int> a;
+    auto n_ops = 0;
+    const auto min_x = x_ids.begin()->F;
+    for (const auto& [x, ids] : x_ids) {
+      const auto m = SZ(ids);
+      if (x == min_x) n_ops += x > 0;
+      else {
+        n_ops += m;
+        FOR(i, 0, m-2) {
+          const auto it = a.upper_bound(ids[i]);
+          n_ops -= (it == a.end() || *it > ids[i+1]);
+        }
+      }
+      for (const auto i : ids) a.insert(i);
+    }
+    return n_ops;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const vi nums{0,5,5};
+    const auto a_out = Solution().minOperations(nums);
+    assert(a_out == 1);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const vi nums{3,1,2,1};
+    const auto a_out = Solution().minOperations(nums);
+    assert(a_out == 3);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const vi nums{1,2,1,2,1,2};
+    const auto a_out = Solution().minOperations(nums);
+    assert(a_out == 4);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const vi nums{0,2};
+    const auto a_out = Solution().minOperations(nums);
+    assert(a_out == 1);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
