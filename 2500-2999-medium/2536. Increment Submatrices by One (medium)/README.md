9bf692702bc06470c2794193de00185f2eca9aa4
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2025-11-16 05:57:36 +0300

    2536. Increment Submatrices by One (medium)

---
+++ 2500-2999-medium/2536. Increment Submatrices by One (medium)/README.md
@@ -0,0 +1,3 @@
+# 2536. Increment Submatrices by One (medium)
+
+https://leetcode.com/problems/increment-submatrices-by-one/
---
+++ 2500-2999-medium/2536. Increment Submatrices by One (medium)/main.cpp
@@ -0,0 +1,100 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ld = long double;
+using ii = std::pair<int, int>;
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vvvi = std::vector<vvi>;
+using vl = std::vector<ll>;
+using vvl = std::vector<vl>;
+using vvvl = std::vector<vvl>;
+using vii = std::vector<ii>;
+using vb = std::vector<bool>;
+using vd = std::vector<ld>;
+using vs = std::vector<std::string>;
+using vc = std::vector<char>;
+
+#define FOR(_i, _a, _b) for (auto _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (auto _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<ld>(_v)
+#define CC(_v) static_cast<char>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+class Solution {
+public:
+  vvi rangeAddQueries(const int n, const vvi& queries) {
+    vvi d(n, vi(n));
+    for (const auto& q : queries) {
+      const auto i1 = q[0], j1 = q[1], i2 = q[2], j2 = q[3];
+      FOR(i, i1, i2) {
+        ++d[i][j1];
+        if (j2 + 1 < n) --d[i][j2 + 1];
+      }
+    }
+    vvi a(n, vi(n));
+    FOR(i, 0, n - 1) {
+      FOR(j, 0, n-1) {
+        a[i][j] = d[i][j] + (j > 0 ? a[i][j - 1] : 0);
+      }
+    }
+    return a;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  /*{
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().solve();
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }*/
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
