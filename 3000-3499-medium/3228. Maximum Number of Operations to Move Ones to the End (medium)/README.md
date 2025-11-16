3a6b83ccb9b4f2b72c7c36da6365aa9f3f10d00f
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2025-11-16 05:55:18 +0300

    3228. Maximum Number of Operations to Move Ones to the End (medium)

---
+++ 3000-3499-medium/3228. Maximum Number of Operations to Move Ones to the End (medium)/README.md
@@ -0,0 +1,3 @@
+# 3228. Maximum Number of Operations to Move Ones to the End (medium)
+
+https://leetcode.com/problems/maximum-number-of-operations-to-move-ones-to-the-end/
---
+++ 3000-3499-medium/3228. Maximum Number of Operations to Move Ones to the End (medium)/main.cpp
@@ -0,0 +1,95 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ld = long double;
+using ii = std::pair<int, int>;
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vvvi = std::vector<vvi>;
+using vl = std::vector<ll>;
+using vvl = std::vector<vl>;
+using vvvl = std::vector<vvl>;
+using vii = std::vector<ii>;
+using vb = std::vector<bool>;
+using vd = std::vector<ld>;
+using vs = std::vector<std::string>;
+using vc = std::vector<char>;
+
+#define FOR(_i, _a, _b) for (auto _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (auto _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<ld>(_v)
+#define CC(_v) static_cast<char>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+class Solution {
+public:
+  int maxOperations(const std::string& s) {
+    const auto n = SZ(s);
+    auto res = 0, c0 = 0;
+    FORD(i, n-1, 0) {
+      if (s[i] == '1') {
+        c0 += (i < n-1 && s[i + 1] == '0');
+        res += (c0);
+      }
+    }
+    return res;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const std::string s("1001101"s);
+    const auto a_out = Solution().maxOperations(s);
+    assert(a_out == 4);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
