a1b654d0b9edde4cac8f2fe44900f58775762875
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2025-11-17 03:18:14 +0300

    2509. Cycle Length Queries in a Tree (hard)

---
+++ 2500-2999-hard/2509. Cycle Length Queries in a Tree (hard)/README.md
@@ -0,0 +1,3 @@
+# 2509. Cycle Length Queries in a Tree (hard)
+
+https://leetcode.com/problems/cycle-length-queries-in-a-tree/
---
+++ 2500-2999-hard/2509. Cycle Length Queries in a Tree (hard)/main.cpp
@@ -0,0 +1,128 @@
+// #pragma GCC optimize("O3,unroll-loops")
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cfloat>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numbers>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ld = long double;
+using ii = std::pair<int, int>;
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vvvi = std::vector<vvi>;
+using vl = std::vector<ll>;
+using vvl = std::vector<vl>;
+using vvvl = std::vector<vvl>;
+using vii = std::vector<ii>;
+using vb = std::vector<bool>;
+using vd = std::vector<ld>;
+using vs = std::vector<std::string>;
+using vc = std::vector<char>;
+
+#define FOR(_i, _a, _b) for (auto _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (auto _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<ld>(_v)
+#define CC(_v) static_cast<char>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+constexpr int MAX_P = 32;
+std::array<int, MAX_P + 1> pa, pb;
+
+int FillP(int x, std::array<int, MAX_P + 1>& p) {
+  auto n = 0;
+  while (x >= 1) {
+    p[n++] = x;
+    x >>= 1;
+  }
+  std::reverse(p.begin(), p.begin() + n);
+  return n;
+}
+
+class Solution {
+public:
+  vi cycleLengthQueries([[maybe_unused]] const int n, const vvi& queries) {
+    vi res(SZ(queries));
+    FOR(qi, 0, SZ(queries) - 1) {
+      const auto a = queries[qi][0], b = queries[qi][1];
+      const auto na = FillP(a, pa);
+      const auto nb = FillP(b, pb);
+      auto i = 0;
+      while (i + 1 < std::min(na, nb) && pa[i+1] == pb[i+1])
+        ++i;
+      const auto da = na - i - 1, db = nb - i - 1;
+      res[qi] = da + db + 1;
+    }
+    return res;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const vvi queries{{5,3},{4,7},{2,3}};
+    const auto a_out = Solution().cycleLengthQueries(3, queries);
+    const vi e_out{4,5,3};
+    assert(a_out == e_out);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  {
+    const auto start_t = std::chrono::high_resolution_clock::now();
+
+    const vvi queries{{1,2}};
+    const auto a_out = Solution().cycleLengthQueries(2, queries);
+    const vi e_out{2};
+    assert(a_out == e_out);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
