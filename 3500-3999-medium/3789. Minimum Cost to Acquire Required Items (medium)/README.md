fc4ce7edf32ddc0be5f901da659bc80e8034a404
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2026-02-21 03:22:52 +0300

    3789. Minimum Cost to Acquire Required Items (medium)

---
+++ 3500-3999-medium/3789. Minimum Cost to Acquire Required Items (medium)/README.md
@@ -0,0 +1,3 @@
+# 3789. Minimum Cost to Acquire Required Items (medium)
+
+https://leetcode.com/problems/minimum-cost-to-acquire-required-items/
---
+++ 3500-3999-medium/3789. Minimum Cost to Acquire Required Items (medium)/main.cpp
@@ -0,0 +1,98 @@
+// #pragma GCC optimize("O3,unroll-loops")
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cfloat>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numbers>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ld = long double;
+using ii = std::pair<int, int>;
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vvvi = std::vector<vvi>;
+using vl = std::vector<ll>;
+using vvl = std::vector<vl>;
+using vvvl = std::vector<vvl>;
+using vii = std::vector<ii>;
+using vb = std::vector<bool>;
+using vd = std::vector<ld>;
+using vs = std::vector<std::string>;
+using vc = std::vector<char>;
+
+#define FOR(_i, _a, _b) for (auto _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (auto _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<ld>(_v)
+#define CC(_v) static_cast<char>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+class Solution {
+public:
+  ll minimumCost(int cost1, int cost2, int cost_both, int need1, int need2) {
+    const auto sum1 = CL(cost1) * need1 + CL(cost2) * need2;
+
+    const auto m = std::min(need1, need2);
+    const auto m1 = need1 - m, m2 = need2 - m;
+    const auto sum2 = CL(cost_both) * m + CL(cost1) * m1 + CL(cost2) * m2;
+    
+    const auto sum3 = CL(cost_both) * std::max(need1, need2);
+    
+    return std::min({sum1, sum2, sum3});
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  /*{
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().solve();
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }*/
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
