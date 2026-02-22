f70010b1e6a2ee3241f39f19fe53253d1cc5edad
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2026-02-22 03:12:50 +0300

    166. Fraction to Recurring Decimal (medium)

---
+++ 0001-0499-medium/166. Fraction to Recurring Decimal (medium)/README.md
@@ -0,0 +1,3 @@
+# 166. Fraction to Recurring Decimal (medium)
+
+https://leetcode.com/problems/fraction-to-recurring-decimal/
---
+++ 0001-0499-medium/166. Fraction to Recurring Decimal (medium)/main.cpp
@@ -0,0 +1,115 @@
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ld = long double;
+using ii = std::pair<int, int>;
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vvvi = std::vector<vvi>;
+using vl = std::vector<ll>;
+using vvl = std::vector<vl>;
+using vvvl = std::vector<vvl>;
+using vii = std::vector<ii>;
+using vb = std::vector<bool>;
+using vd = std::vector<ld>;
+using vs = std::vector<std::string>;
+using vc = std::vector<char>;
+
+#define FOR(_i, _a, _b) for (auto _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (auto _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<ld>(_v)
+#define CC(_v) static_cast<char>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+#define PB push_back
+
+class Solution {
+public:
+  std::string fractionToDecimal(ll num, ll den) {
+    if (num == 0)
+      return "0"s;
+
+    std::string s;
+    if ((num < 0) ^ (den < 0))
+      s.push_back('-');
+
+    num = std::abs(num), den = std::abs(den);
+    s += std::to_string(num / den);
+    auto mod = num % den;
+    if (mod == 0)
+      return s;
+
+    s.push_back('.');
+    std::unordered_map<ll, int> memo;
+    while (mod != 0) {
+      const auto it = memo.find(mod);
+      if (it != memo.end()) {
+        s.insert(it->S, "("s);
+        s.push_back(')');
+        break;
+      }
+      memo[mod] = SZ(s);
+      mod *= 10;
+      s += std::to_string(mod / den);
+      mod %= den;
+    }
+
+    return s;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  /*{
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().solve();
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }*/
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
