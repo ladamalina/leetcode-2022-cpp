1fc83df1f293bf4b296c233342705d68b613b1a4
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2026-02-22 03:27:05 +0300

    3577. Count the Number of Computer Unlocking Permutations (medium)

---
+++ 3500-3999-medium/3577. Count the Number of Computer Unlocking Permutations (medium)/README.md
@@ -0,0 +1,3 @@
+# 3577. Count the Number of Computer Unlocking Permutations (medium)
+
+https://leetcode.com/problems/count-the-number-of-computer-unlocking-permutations
---
+++ 3500-3999-medium/3577. Count the Number of Computer Unlocking Permutations (medium)/main.cpp
@@ -0,0 +1,108 @@
+// #pragma GCC optimize("O3,unroll-loops")
+// #include <bits/stdc++.h>
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cfloat>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numbers>
+#include <numeric>
+#include <optional>
+#include <random>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+using namespace std::literals;
+
+using ll = long long;
+using ld = long double;
+using ii = std::pair<int, int>;
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vvvi = std::vector<vvi>;
+using vl = std::vector<ll>;
+using vvl = std::vector<vl>;
+using vvvl = std::vector<vvl>;
+using vii = std::vector<ii>;
+using vb = std::vector<bool>;
+using vd = std::vector<ld>;
+using vs = std::vector<std::string>;
+using vc = std::vector<char>;
+
+#define FOR(_i, _a, _b) for (auto _i = (_a); _i <= (_b); ++(_i))
+#define FORD(_i, _a, _b) for (auto _i = (_a); _i >= (_b); --(_i))
+#define RNG(_l) (_l).begin(), (_l).end()
+#define SORT(_l) std::sort((_l).begin(), (_l).end())
+#define CI(_v) static_cast<int>(_v)
+#define CL(_v) static_cast<ll>(_v)
+#define CD(_v) static_cast<ld>(_v)
+#define CC(_v) static_cast<char>(_v)
+#define SZ(_v) static_cast<int>((_v).size())
+#define F first
+#define S second
+
+constexpr int MOD = 1e9 + 7;
+
+class Solution {
+public:
+  int countPermutations(const vi& comp) {
+    const auto n = SZ(comp);
+    int min_comp = INT_MAX, min_comp_i = -1, min_comp_cnt = 0;
+    FOR(i, 0, n-1) {
+      if (comp[i] < min_comp) {
+        min_comp = comp[i];
+        min_comp_cnt = 1;
+        min_comp_i = i;
+      } else if (min_comp == comp[i]) {
+        ++min_comp_cnt;
+      }
+    }
+    if (min_comp_cnt > 1 || min_comp_i != 0)
+      return 0;
+
+    ll res = 1;
+    FOR(i, 2, n-1) res = (res * i) % MOD;
+    return res;
+  }
+};
+
+[[maybe_unused]] void TestSolution() {
+  /*{
+    const auto start_t = std::chrono::high_resolution_clock::now();
+  
+    const auto a_out = Solution().solve();
+    assert(a_out == 6);
+  
+    const auto end_t = std::chrono::high_resolution_clock::now();
+    const auto total_t = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+    std::cerr << total_t << " ms"sv << std::endl;
+  }*/
+  std::cerr << "TestSolution OK"sv << std::endl;
+}
+
+int main() {
+#ifndef NDEBUG
+  TestSolution();
+#endif
+  return 0;
+}
